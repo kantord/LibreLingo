acfb5f4cb95cb0af77a40b26413ae12f7cb0d9a6
Dániel Kántor <git@daniel-kantor.com>
2019-11-20 23:39:58 +0100

    Allow validation of solutions

--- src/components/DeckChallenge.svelte
+++ src/components/DeckChallenge.svelte
@@ -9,22 +9,44 @@ export let alternativeChallenges;
 export let resolveChallenge;
 let selectedOption = null;
 let options = null;
+let submitted = false;
 
-$: {
-	options = shuffle([currentChallenge, ...shuffle(alternativeChallenges).slice(0, 2)]);
+$: correctOption = {
+	...currentChallenge,
+	correct: true
 }
 
-$: submitSolution = () => {
+$: incorrectOptions = alternativeChallenges.map(challenge => ({
+	...challenge,
+	correct: false
+}))
+
+$: {
+	options = shuffle([correctOption, ...shuffle(incorrectOptions).slice(0, 2)]);
+}
+
+$: nextChallenge = () => {
 	selectedOption = null;
+	submitted = false;
 	resolveChallenge();
+}
+
+$: submitChallenge = () => {
+	submitted = true;
 }
 
 
 onMount(() => {
 	hotkeys('1,2,3,enter', (_, { key }) => {
 		if (key === "enter") {
-			submitSolution();
+			if (submitted) {
+				nextChallenge()
+			} else {
+				submitChallenge();
+			}
 		} else {
+			if (submitted) return;
+
 			selectedOption = parseInt(key) - 1;
 		}
 	});
@@ -34,15 +56,49 @@ onMount(() => {
 
 <h1>Select {currentChallenge.meaningInSourceLanguage}!</h1>
 
-<form on:submit|preventDefault={submitSolution}>
-	<OptionDeck options={options} bind:selectedOption={selectedOption} />
+<form on:submit|preventDefault={submitChallenge}>
+	<OptionDeck options={options} bind:selectedOption={selectedOption} disabled={submitted} />
 
+	{#if !submitted }
 	  <button type="submit">Submit</button>
+	{/if}
+
+	{#if submitted }
+		{#if options[selectedOption].correct }
+			<div class="result correct">
+				Correct solution!
+				<button type="submit" on:click={nextChallenge}>Continue</button>
+			</div>
+		{/if}
+
+		{#if !options[selectedOption].correct }
+			<div class="result incorrect">
+				Incorect solution!
+				<button type="submit" on:click={nextChallenge}>Continue</button>
+			</div>
+		{/if}
+	{/if}
 </form>
 
 <style>
 	h1 {
 		text-align: center;
+	}
+
+	.correct {
+		color: green;
+	}
+
+	.incorrect {
+		color: red;
+	}
+
+	.result {
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		height: 20vh;
 	}
 </style>
 
--- src/components/OptionDeck.svelte
+++ src/components/OptionDeck.svelte
@@ -2,12 +2,13 @@
 	import OptionCard from '../components/OptionCard.svelte'
 	export let options;
 	export let selectedOption;
+	export let disabled;
 </script>
 
 <ul class="options">
 {#each options as { picture, meaningInSourceLanguage, formInTargetLanguage }, i}
 	<label for={i}>
-		<input type="radio" bind:group={selectedOption} value={i} name={i} id={i} >
+		<input type="radio" bind:group={selectedOption} value={i} name={i} id={i} disabled={disabled} >
 		<OptionCard active={selectedOption===i} picture={picture} number={i + 1} formInTargetLanguage={formInTargetLanguage} />
 	</label>
 {/each}
