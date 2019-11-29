66cfce5138a1585414f75d526296ddb07fc48428
Dániel Kántor <git@daniel-kantor.com>
2019-11-29 11:18:44 +0100

    show progress in skill

--- src/routes/skill.svelte
+++ src/routes/skill.svelte
@@ -5,49 +5,60 @@
 
   const rawChallenges = [
     {
+      id: 4,
       pictures: ["lion1.jpg", "lion2.jpg", "lion3.jpg"],
       meaningInSourceLanguage: "lion",
       formInTargetLanguage: "león"
     },
     {
+      id: 3,
       pictures: ["cat1.jpg", "cat2.jpg", "cat3.jpg"],
       meaningInSourceLanguage: "cat",
       formInTargetLanguage: "gato"
     },
     {
+      id: 42,
       pictures: ["dog1.jpg", "dog2.jpg", "dog3.jpg"],
       meaningInSourceLanguage: "dog",
       formInTargetLanguage: "perro"
     },
     {
+      id: 888,
       pictures: ["duck1.jpg", "duck2.jpg", "duck3.jpg"],
       meaningInSourceLanguage: "duck",
       formInTargetLanguage: "pato"
     },
     {
+      id: 202,
       pictures: ["bear1.jpg", "bear2.jpg", "bear3.jpg"],
       meaningInSourceLanguage: "bear",
       formInTargetLanguage: "oso"
     }
   ];
 
-  let currentChallengeId = 0;
   let challenges = shuffle(rawChallenges);
-  $: currentChallenge = challenges[currentChallengeId];
+  let remainingChallenges = [...challenges];
+  let currentChallenge = remainingChallenges.shift();
+  let solvedChallenges = [];
+  let progress = 0;
   $: alternativeChallenges = challenges.filter(
-    (_, id) => id !== currentChallengeId
+    ({ id }) => id !== currentChallenge.id
   );
 
   $: registerResult = isCorrect => {
     if (isCorrect) {
       sound.correct.play();
+      solvedChallenges.push(currentChallenge);
     } else {
       sound.wrong.play();
+      remainingChallenges.push(currentChallenge);
     }
+
+    progress = solvedChallenges.length / challenges.length;
   };
 
   $: resolveChallenge = () => {
-    currentChallengeId++;
+    currentChallenge = remainingChallenges.shift();
   };
 
   const preloadImage = imageName => {
@@ -62,7 +73,7 @@
   <title>Exercise</title>
 </svelte:head>
 
-<p>Progress: {currentChallengeId}</p>
+<p>Progress: {progress * 100}%</p>
 
 <DeckChallenge
   {currentChallenge}
