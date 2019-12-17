b7fd41aad1585907af4703174032bf53e8bc6366
Dániel Kántor <git@daniel-kantor.com>
2019-12-17 22:16:42 +0100

    fix cypress test

--- cypress/integration/coursePage.js
+++ cypress/integration/coursePage.js
@@ -2,24 +2,10 @@ describe("Course page", () => {
     const COURSE_PAGE_URL = "/"
     describe("Open skill page", () => {
         beforeEach(() => {
-            cy.visit(COURSE_PAGE_URL)
             cy.window().then(window => {
-                const db = window._DB
-                db.allDocs()
-                    .then(function(result) {
-                        return Promise.all(
-                            result.rows.map(function(row) {
-                                return db.remove(row.id, row.value.rev)
-                            })
-                        )
-                    })
-                    .then(function() {
-                        console.log("database erased")
-                    })
-                    .catch(function(err) {
-                        console.log("database couldn't be erased", err)
-                    })
+                cy.wrap(window.indexedDB.deleteDatabase("_pouch_localData"))
             })
+            cy.visit(COURSE_PAGE_URL)
         })
 
         it("Animals skill should not be visible", () => {
@@ -33,11 +19,9 @@ describe("Course page", () => {
         it("Food skill should be visible when its available based on database", () => {
             cy.window().then(window => {
                 const db = window._DB
-                db.put({ _id: "foo_bar" }).then(function() {
-                    console.log("data put into db")
-                })
-                cy.reload()
+                cy.wrap(db.put({ _id: "foo_bar" }))
             })
+            cy.reload()
             cy.contains(/Food/).should("be.visible")
         })
     })
