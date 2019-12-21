c138ec9f218e77032220bc6bb9170f337fea087b
Dániel Kántor <gitlab@daniel-kantor.com>
2019-12-21 19:09:55 +0000

    Updates

---
+++ client/845ebf08d4044de6d101/12.12.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{54:function(a){a.exports=JSON.parse('{"languageName":"Test Language"}')}}]);
---
+++ client/845ebf08d4044de6d101/9.9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{58:function(n){n.exports=JSON.parse('[{"id":6663,"type":"shortInput","meaningInSourceLanguage":"dog","formInTargetLanguage":["perro","el perro","can"]}]')}}]);
