80a82517bd793b5bbe7dc0df434b0a734dfe392d
Dániel Kántor <git@daniel-kantor.com>
2020-01-02 00:44:10 +0100

    course editor: add LearnWord model

--- course_editor/course/models.py
+++ course_editor/course/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class LearnWord(models.Model):
+    meaningInSourceLanguage = models.TextField()
+    formInTargetLanguage = models.TextField()
