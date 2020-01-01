57b3ed9baf1af737739f1cc74094da66838709ae
Dániel Kántor <git@daniel-kantor.com>
2020-01-02 00:50:40 +0100

    course editor: some eye candy

--- course_editor/course/models.py
+++ course_editor/course/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 
 class LearnWord(models.Model):
-    meaningInSourceLanguage = models.TextField()
-    formInTargetLanguage = models.TextField()
+    class Meta:
+        verbose_name = "Learn a new word"
+    meaningInSourceLanguage = models.TextField(verbose_name="Meaning in source language")
+    formInTargetLanguage = models.TextField(verbose_name="Meaning in target language")
--- course_editor/course_editor/urls.py
+++ course_editor/course_editor/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_header = "LibreLingo Course Editor"
+admin.site.site_title = "LibreLingo Course Editor"
+admin.site.index_title = "Welcome to LibreLingo Course Editor"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
