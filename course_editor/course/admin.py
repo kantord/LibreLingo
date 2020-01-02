2c32256cbad7ff55a45aa71de059971286510c6b
Dániel Kántor <git@daniel-kantor.com>
2020-01-02 23:36:51 +0100

    course editor: add models

--- course_editor/course/admin.py
+++ course_editor/course/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
 from .models import LearnWord
+from .models import Course
+from .models import Module
+from .models import Skill
 
 admin.site.register(LearnWord)
+admin.site.register(Course)
+admin.site.register(Module)
+admin.site.register(Skill)
--- course_editor/course/models.py
+++ course_editor/course/models.py
@@ -6,3 +6,23 @@ class LearnWord(models.Model):
         verbose_name = "Learn a new word"
     meaningInSourceLanguage = models.TextField(verbose_name="Meaning in source language")
     formInTargetLanguage = models.TextField(verbose_name="Meaning in target language")
+
+
+class Course(models.Model):
+    class Meta:
+        verbose_name = "Language course"
+    language_name = models.TextField(verbose_name="Language name")
+
+
+class Module(models.Model):
+    class Meta:
+        verbose_name = "Module"
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    name = models.TextField(verbose_name="Language name")
+
+
+class Skill(models.Model):
+    class Meta:
+        verbose_name = "Skill"
+    module = models.ForeignKey('Module', on_delete=models.CASCADE)
+    name = models.TextField(verbose_name="Skill")
