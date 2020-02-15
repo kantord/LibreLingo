1f2810cb1b0485b8c01506458aed94a22b806e25
Daniel Kantor <git@daniel-kantor.com>
2020-02-15 10:09:51 +0100

    Get rid of console warnings (#107)

--- src/components/ChallengeScreen.svelte
+++ src/components/ChallengeScreen.svelte
@@ -1,105 +1,105 @@
 <script>
-    import sound from "../media/sound"
-    import DeckChallenge from "./DeckChallenge"
-    import ShortInputChallenge from "./ShortInputChallenge"
-    import ListeningChallenge from "./ListeningChallenge"
-    import FanfareScreen from "./FanfareScreen"
-    import ProgressBar from "./ProgressBar"
-    import shuffle from "lodash.shuffle"
-    import { fade } from "svelte/transition"
+  import sound from "../media/sound";
+  import DeckChallenge from "./DeckChallenge";
+  import ShortInputChallenge from "./ShortInputChallenge";
+  import ListeningChallenge from "./ListeningChallenge";
+  import FanfareScreen from "./FanfareScreen";
+  import ProgressBar from "./ProgressBar";
+  import shuffle from "lodash.shuffle";
+  import { fade } from "svelte/transition";
 
-    export let rawChallenges
-    export let languageName
-    export let languageCode
-    export let sortChallengeGroups
-    export let courseURL
-    let challenges = sortChallengeGroups(shuffle(rawChallenges))
-    let remainingChallenges = [...challenges]
-    let currentChallenge = remainingChallenges.shift()
-    let solvedChallenges = []
-    let progress = 0
+  export let rawChallenges;
+  export let languageName;
+  export let languageCode;
+  export let sortChallengeGroups;
+  export let courseURL;
+  let challenges = sortChallengeGroups(shuffle(rawChallenges));
+  let remainingChallenges = [...challenges];
+  let currentChallenge = remainingChallenges.shift();
+  let solvedChallenges = [];
+  let progress = 0;
 
-    const preloadImage = imageName => {
-        if (typeof Image === "undefined") return
-        new Image().src = `images/${imageName}`
+  const preloadImage = imageName => {
+    if (typeof Image === "undefined") return;
+    new Image().src = `images/${imageName}`;
+  };
+
+  challenges
+    .filter(({ type }) => type === "card")
+    .map(({ pictures }) => pictures.map(preloadImage));
+
+  $: alternativeChallenges =
+    currentChallenge &&
+    challenges.filter(({ id }) => id !== currentChallenge.id);
+
+  $: registerResult = isCorrect => {
+    if (isCorrect) {
+      sound.correct.play();
+      solvedChallenges.push(currentChallenge);
+    } else {
+      sound.wrong.play();
+      remainingChallenges.push(currentChallenge);
     }
 
-    challenges
-        .filter(({ type }) => type === "card")
-        .map(({ pictures }) => pictures.map(preloadImage))
+    progress = solvedChallenges.length / challenges.length;
+  };
 
-    $: alternativeChallenges =
-        currentChallenge &&
-        challenges.filter(({ id }) => id !== currentChallenge.id)
-
-    $: registerResult = isCorrect => {
-        if (isCorrect) {
-            sound.correct.play()
-            solvedChallenges.push(currentChallenge)
-        } else {
-            sound.wrong.play()
-            remainingChallenges.push(currentChallenge)
-        }
-
-        progress = solvedChallenges.length / challenges.length
+  $: resolveChallenge = () => {
+    if (remainingChallenges) {
+      currentChallenge = remainingChallenges.shift();
     }
-
-    $: resolveChallenge = () => {
-        if (remainingChallenges) {
-            currentChallenge = remainingChallenges.shift()
-        }
-    }
+  };
 </script>
 
 {#if currentChallenge}
 
-    <div class="container">
-        <section class="section">
-            <ProgressBar value="{progress}" />
-            {#each challenges as challenge, i (challenge.id)}
-                {#if challenge.id === currentChallenge.id}
-                    <div
-                        out:fade="{{ duration: 300 }}"
-                        in:fade="{{ duration: 300, delay: 300 }}">
-                        {#if challenge.type === 'cards'}
-                            <DeckChallenge
-                                {currentChallenge}
-                                {alternativeChallenges}
-                                {resolveChallenge}
-                                {registerResult} />
-                        {/if}
-                        {#if challenge.type === 'shortInput'}
-                            <ShortInputChallenge
-                                {languageName}
-                                {languageCode}
-                                {registerResult}
-                                {resolveChallenge}
-                                {challenge} />
-                        {/if}
-                        {#if challenge.type === 'listeningExercise'}
-                            <ListeningChallenge
-                                {languageName}
-                                {languageCode}
-                                {registerResult}
-                                {resolveChallenge}
-                                {challenge} />
-                        {/if}
-                    </div>
-                {/if}
-            {/each}
+  <div class="container">
+    <section class="section">
+      <ProgressBar value="{progress}" />
+      {#each challenges as challenge, i (challenge.id)}
+        {#if challenge.id === currentChallenge.id}
+          <div
+            out:fade="{{ duration: 300 }}"
+            in:fade="{{ duration: 300, delay: 300 }}">
+            {#if challenge.type === 'cards'}
+              <DeckChallenge
+                {currentChallenge}
+                {alternativeChallenges}
+                {resolveChallenge}
+                {registerResult} />
+            {/if}
+            {#if challenge.type === 'shortInput'}
+              <ShortInputChallenge
+                {languageName}
+                {languageCode}
+                {registerResult}
+                {resolveChallenge}
+                {challenge} />
+            {/if}
+            {#if challenge.type === 'listeningExercise'}
+              <ListeningChallenge
+                {languageName}
+                {languageCode}
+                {registerResult}
+                {resolveChallenge}
+                {challenge} />
+            {/if}
+          </div>
+        {/if}
+      {/each}
 
-        </section>
-    </div>
+    </section>
+  </div>
 {/if}
 
 {#if !currentChallenge}
-    <div class="container">
-        <FanfareScreen {courseURL} {rawChallenges} />
-    </div>
+  <div class="container">
+    <FanfareScreen {courseURL} {rawChallenges} />
+  </div>
 {/if}
 
 <style>
-    .section {
-        padding: 1.5em;
-    }
+  .section {
+    padding: 1.5em;
+  }
 </style>
--- src/components/ListeningChallenge.svelte
+++ src/components/ListeningChallenge.svelte
@@ -1,151 +1,150 @@
 <script>
-    import { slide } from "svelte/transition"
-    import { onMount } from "svelte"
-    import hotkeys from "hotkeys-js"
-    import levenshtein from "js-levenshtein"
-    import shuffle from "lodash.shuffle"
-    import ChallengePanel from "./ChallengePanel"
-    import playVoice from "../media/voice"
+  import { slide } from "svelte/transition";
+  import { onMount } from "svelte";
+  import hotkeys from "hotkeys-js";
+  import levenshtein from "js-levenshtein";
+  import shuffle from "lodash.shuffle";
+  import ChallengePanel from "./ChallengePanel";
+  import playVoice from "../media/voice";
 
-    export let challenge
-    export let registerResult
-    export let resolveChallenge
-    export let languageName
-    export let languageCode
-    let answer = null
-    let submitted = false
-    let correct = null
-    let spellingSuggestion = ""
-    let inputFieldRef = null
+  export let challenge;
+  export let registerResult;
+  export let resolveChallenge;
+  export let languageName;
+  export let languageCode;
+  let answer = null;
+  let submitted = false;
+  let correct = null;
+  let spellingSuggestion = "";
+  let inputFieldRef = null;
 
-    $: submitChallenge = () => {
-        if (!answer) return
-        if (submitted) return
-        const form = challenge.answer
-        correct = false
+  $: submitChallenge = () => {
+    if (!answer) return;
+    if (submitted) return;
+    const form = challenge.answer;
+    correct = false;
 
-        if (
-            levenshtein(
-                answer
-                    .toLowerCase()
-                    .replace(/^\s+|\s+$/g, "")
-                    .replace(/\s+/g, " "),
-                form.toLowerCase()
-            ) <= 1
-        ) {
-            correct = true
-            spellingSuggestion =
-                form
-                    .replace(/^\s+|\s+$/g, "")
-                    .replace(/\s+/g, " ")
-                    .toLowerCase() === answer.toLowerCase()
-                    ? ""
-                    : `You made a small error. Correct spelling: ${form}`
-        }
-
-        registerResult(correct)
-        inputFieldRef.blur()
-        submitted = true
+    if (
+      levenshtein(
+        answer
+          .toLowerCase()
+          .replace(/^\s+|\s+$/g, "")
+          .replace(/\s+/g, " "),
+        form.toLowerCase()
+      ) <= 1
+    ) {
+      correct = true;
+      spellingSuggestion =
+        form
+          .replace(/^\s+|\s+$/g, "")
+          .replace(/\s+/g, " ")
+          .toLowerCase() === answer.toLowerCase()
+          ? ""
+          : `You made a small error. Correct spelling: ${form}`;
     }
 
-    $: finishChallenge = () => {
-        answer = null
-        submitted = false
-        resolveChallenge()
-    }
+    registerResult(correct);
+    inputFieldRef.blur();
+    submitted = true;
+  };
 
-    const focusMe = el => {
-        setTimeout(() => {
-            if (el.disabled) {
-                el.blur()
-            } else {
-                el.focus()
-            }
-        }, 1)
-    }
+  $: finishChallenge = () => {
+    answer = null;
+    submitted = false;
+    resolveChallenge();
+  };
 
-    const playChallengeVoice = () => playVoice(challenge.audio)
+  const focusMe = el => {
+    setTimeout(() => {
+      if (el.disabled) {
+        el.blur();
+      } else {
+        el.focus();
+      }
+    }, 1);
+  };
 
-    onMount(() => {
-        playChallengeVoice()
-        hotkeys.unbind("enter")
-        hotkeys("enter", () => {
-            if (submitted) {
-                finishChallenge()
-            } else {
-                submitChallenge()
-            }
-        })
-    })
+  const playChallengeVoice = () => playVoice(challenge.audio);
+
+  onMount(() => {
+    playChallengeVoice();
+    hotkeys.unbind("enter");
+    hotkeys("enter", () => {
+      if (submitted) {
+        finishChallenge();
+      } else {
+        submitChallenge();
+      }
+    });
+  });
 </script>
 
 <form on:submit|preventDefault="{submitChallenge}">
-    <div class="section">
-        <p
-            class="is-size-1 is-size-2-tablet is-size-4-mobile has-text-centered">
-            Type what you hear
-        </p>
+  <div class="section">
+    <p class="is-size-1 is-size-2-tablet is-size-4-mobile has-text-centered">
+      Type what you hear
+    </p>
+  </div>
+
+  <div class="columns">
+
+    <div class="column is-1">
+
+      <button
+        class="button is-large is-primary"
+        on:click="{playChallengeVoice}">
+        <span class="icon is-medium">
+          <i class="fas fa-volume-up "></i>
+        </span>
+      </button>
     </div>
 
-    <div class="columns">
+    <div class="column">
+      <input
+        tabindex="0"
+        data-test="answer"
+        type="text"
+        class="input"
+        autofocus
+        placeholder="Type your answer…"
+        disabled="{submitted}"
+        spellcheck="false"
+        lang="{languageCode}"
+        use:focusMe
+        bind:this="{inputFieldRef}"
+        bind:value="{answer}" />
 
-        <div class="column is-1">
-
-            <button
-                class="button is-large is-primary"
-                on:click="{playChallengeVoice}">
-                <span class="icon is-medium">
-                    <i class="fas fa-volume-up "></i>
-                </span>
-            </button>
-        </div>
-
-        <div class="column">
-            <input
-                tabindex="0"
-                data-test="answer"
-                type="text"
-                class="input"
-                autofocus
-                placeholder="Type your answer…"
-                disabled="{submitted}"
-                spellcheck="false"
-                lang="{languageCode}"
-                use:focusMe
-                bind:this="{inputFieldRef}"
-                bind:value="{answer}" />
-
-        </div>
     </div>
+  </div>
 
-    {#if answer && !submitted}
-        <ChallengePanel message="" buttonText="Submit" submit />
-    {/if}
+  {#if answer && !submitted}
+    <ChallengePanel message="" buttonText="Submit" submit />
+  {/if}
 
-    {#if submitted}
-        {#if !correct}
-            <ChallengePanel
-                message="Incorrect solution!"
-                messageDetail="{`Correct answer: ${challenge.answer}`}"
-                buttonText="Continue"
-                incorrect
-                buttonAction="{finishChallenge}" />
-        {/if}
-        {#if correct}
-            <ChallengePanel
-                message="Correct solution!"
-                messageDetail="{spellingSuggestion || `Meaning: "${challenge.meaning}"`}"
-                buttonText="Continue"
-                correct
-                buttonAction="{finishChallenge}" />
-        {/if}
+  {#if submitted}
+    {#if !correct}
+      <ChallengePanel
+        message="Incorrect solution!"
+        messageDetail="{`Correct answer: ${challenge.answer}`}"
+        buttonText="Continue"
+        incorrect
+        buttonAction="{finishChallenge}" />
     {/if}
+    {#if correct}
+      <ChallengePanel
+        message="Correct solution!"
+        messageDetail="{spellingSuggestion || `Meaning: "${challenge.meaning}"`}"
+        buttonText="Continue"
+        correct
+        buttonAction="{finishChallenge}" />
+    {/if}
+  {/if}
 
 </form>
 
 <style>
-    .card {
-        max-width: 16em;
-        margin: auto;
-    }
+  .card {
+    max-width: 16em;
+    margin: auto;
+  }
 </style>
