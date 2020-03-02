28be861572f1ce6ac9bec4a952577823290ff49c
Dániel Kántor <git@daniel-kantor.com>
2020-03-02 17:46:16 +0100

    Move fetchPhotos to scripts/

--- scripts/fetchPhotos.sh
+++ scripts/fetchPhotos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 tail -n +2 ./image_attributions.csv | while read -r photo_description; do \
-    ./fetchPhoto.sh $photo_description; \
+    ./scripts/fetchPhoto.sh $photo_description; \
 done
