f05fc3b43df218401a138776734de91c5ad7184b
Dániel Kántor <gitlab@daniel-kantor.com>
2020-03-15 18:19:07 +0000

    Updates

---
+++ client/3a8a9ba2111626201e0d/29.29.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{254:function(e){e.exports=JSON.parse('[{"id":663,"type":"shortInput","meaningInSourceLanguage":"foo","formInTargetLanguage":["leche","la leche"]}]')}}]);
