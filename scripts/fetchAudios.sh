66de03c335d83f6a7993cc6ba58624c16f63d62c
Dániel Kántor <git@daniel-kantor.com>
2020-03-15 12:44:16 +0100

    Add "Agua, Por Favor" audio clip #174
    fixes #174

--- scripts/fetchAudios.sh
+++ scripts/fetchAudios.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cat ./src/audios_to_fetch.csv | while read -r audio_description; do \
+cat ./src/audios_to_fetch.csv <(echo "") | while read -r audio_description; do \
     ./scripts/fetchAudio.sh "$audio_description"; \
 done
