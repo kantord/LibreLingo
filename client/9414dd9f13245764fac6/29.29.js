7feaaaed8cec1315045e2f689f34b385be046439
Dániel Kántor <gitlab@daniel-kantor.com>
2020-03-23 22:06:03 +0000

    Updates

--- client/9414dd9f13245764fac6/29.29.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{254:function(o){o.exports=JSON.parse('[{"id":6663,"type":"shortInput","meaningInSourceLanguage":"dog","pictures":["dog1.jpg","dog2.jpg","dog3.jpg"],"formInTargetLanguage":["perro","el perro","can"]}]')}}]);
