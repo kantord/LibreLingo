0698004ce7a73805ad83e43de0496d68e6175e7c
Dániel Kántor <git@daniel-kantor.com>
2020-03-29 22:00:53 +0200

    perf: optimize font awesome loading

--- src/client.js
+++ src/client.js
@@ -1,9 +1,8 @@
 import * as sapper from "@sapper/app"
 import "./mystyles.scss"
 import "@openfonts/josefin-sans_all"
-import "@fortawesome/fontawesome-free/js/all.min.js"
 import "./i18n"
 
 sapper.start({
-    target: document.querySelector("#sapper")
+    target: document.querySelector("#sapper"),
 })
