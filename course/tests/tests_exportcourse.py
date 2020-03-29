7fe39a944230ea5fd2fa56e8177482d24b9bb40c
iasonasma <44037134+iasonasma@users.noreply.github.com>
2020-03-29 22:35:32 +0600

    test: test exportcourse argument type

--- course/tests/tests_exportcourse.py
+++ course/tests/tests_exportcourse.py
@@ -4,6 +4,8 @@ from django.core import management
 from io import StringIO
 import sys
 
+from django.utils import translation
+
 
 class CommandTests(TestCase):
     databases = '__all__'
@@ -17,3 +19,7 @@ class CommandTests(TestCase):
     def test_check_course_does_not_exists(self):
         with self.assertRaisesMessage(CommandError, 'Course "645343" does not exist'):
             management.call_command('exportcourse', 645343, stdout=StringIO(), verbosity=3)
+
+    def test_argument_type(self):
+        with self.assertRaisesMessage(CommandError, "Error: argument course_id: invalid int value: 'dgdnfgn'"):
+            management.call_command('exportcourse', "dgdnfgn", stdout=StringIO(), verbosity=3)
