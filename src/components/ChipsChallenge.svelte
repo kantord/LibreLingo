fe3a5bb03a3462d39f8859864e47d8d5b2289f7e
Dániel Kántor <git@daniel-kantor.com>
2020-03-30 10:43:14 +0200

    refactor(chips challenge): extract <Phrase /> component

--- src/components/ChipsChallenge.svelte
+++ src/components/ChipsChallenge.svelte
@@ -3,6 +3,7 @@
   import shuffle from "lodash.shuffle";
   import { writable, get } from "svelte/store";
   import ChallengePanel from "./ChallengePanel";
+  import Phrase from "./Phrase";
 
   const swapArrayElements = function(arr, indexA, indexB) {
     const temp = arr[indexA];
@@ -87,15 +88,7 @@
   <div class="section">
     <p class="is-size-1 is-size-2-tablet is-size-4-mobile has-text-centered">
       Translate
-      <b class="phrase">
-	      {#each challenge.phrase as { word, definition }}
-		      {#if definition}
-			      <span class="has-tooltip-bottom" data-tooltip="{definition}">{word}</span>
-		      {:else}
-			      <span>{word}</span>
-		      {/if}
-	      {/each}
-      </b>
+      <Phrase  phrase={challenge.phrase} />
     </p>
   </div>
 
@@ -150,21 +143,6 @@
 
 <style>
   @import "../variables";
-
-  .phrase span {
-	  margin: 0 .15em;
-
-	  &.has-tooltip-bottom {
-		  text-decoration: wavy underline rgba($blue, .5);
-	  }
-
-	  &:first-child {
-		  margin-left: 0;
-	  }
-	  &:last-child {
-		  margin-left: 0;
-	  }
-  }
 
   .chip {
     user-select: none;
