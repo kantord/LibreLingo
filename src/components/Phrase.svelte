ec0e4adb206610f219c165bcaf494191f7671591
Dániel Kántor <git@daniel-kantor.com>
2020-03-30 10:43:14 +0200

    refactor(chips challenge): extract <Phrase /> component

---
+++ src/components/Phrase.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let phrase
+</script>
+
+<b class="phrase">
+      {#each phrase as { word, definition }}
+	      {#if definition}
+		      <span class="has-tooltip-bottom" data-tooltip="{definition}">{word}</span>
+	      {:else}
+		      <span>{word}</span>
+	      {/if}
+      {/each}
+</b>
+
+<style>
+  @import "../variables";
+
+  .phrase span {
+	  margin: 0 .15em;
+
+	  &.has-tooltip-bottom {
+		  text-decoration: wavy underline rgba($blue, .5);
+	  }
+
+	  &:first-child {
+		  margin-left: 0;
+	  }
+	  &:last-child {
+		  margin-left: 0;
+	  }
+  }
+</style>
