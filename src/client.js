fef7cb6c880dd98128c5d97fc11a08bbdb5b0978
Dániel Kántor <git@daniel-kantor.com>
2020-04-10 23:10:06 +0200

    feat: implement stale skills (spaced repetition) fixes #312
    .
    
    .
    
    .

--- src/client.js
+++ src/client.js
@@ -3,10 +3,14 @@ import "./mystyles.scss"
 import "@openfonts/josefin-sans_all"
 import "./i18n"
 import { library, dom } from "@fortawesome/fontawesome-svg-core"
-import { faVolumeUp } from "@fortawesome/free-solid-svg-icons"
-import { faCheckSquare } from "@fortawesome/free-solid-svg-icons"
+import {
+    faVolumeUp,
+    faCheckSquare,
+    faDumbbell
+} from "@fortawesome/free-solid-svg-icons"
 library.add(faVolumeUp)
 library.add(faCheckSquare)
+library.add(faDumbbell)
 dom.watch()
 
 sapper.start({
