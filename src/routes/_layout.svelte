cdf1776072f3a08cd0e4eb3af1fbfeffd94ccc18
Dániel Kántor <git@daniel-kantor.com>
2020-04-12 17:52:05 +0200

    chore: hide loading screen in cypress to increase performance

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,12 +1,17 @@
 <script context="module">
   import { waitLocale } from "svelte-i18n"
-
   export async function preload() {
     return waitLocale()
   }
 </script>
 
-{#if process.browser !== true}
+<script>
+  import { stores } from "@sapper/app"
+  const { preloading, page, session } = stores()
+  let userAgent = $session["user-agent"]
+</script>
+
+{#if process.browser !== true && userAgent !== 'cypress'}
   <div class="pageloader is-active">
     <span class="title">LibreLingo</span>
   </div>
