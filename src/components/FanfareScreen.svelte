2d1406a4193efe249cd7cbcc8a32260ddc3186bb
Dániel Kántor <git@daniel-kantor.com>
2020-04-15 19:49:37 +0200

    feat: update fanfare screen design refactor(fanfare screen tests): refactor tests to use cucumber
    feat: add twitter button to <FanfareScreen />
    
    .
    
    feat: add mascot to fanfare screen
    
    fixes #89
    
    feat(fanfarescreen): align Twitter button to the bottom
    
    feat: add twitter button to fanfare screen

--- src/components/FanfareScreen.svelte
+++ src/components/FanfareScreen.svelte
@@ -1,9 +1,11 @@
 <script>
+  import { scale } from "svelte/transition"
   import db from "../db"
   import sound from "../media/sound"
   import hotkeys from "hotkeys-js"
   import { onMount } from "svelte"
   import { goto } from "@sapper/app"
+  import Icon from "./Icon"
 
   export let rawChallenges
   export let courseURL
@@ -47,14 +49,44 @@
   })
 </script>
 
-<section class="hero">
+<section class="hero is-fullheight-with-navbar">
   <div class="hero-body">
     <div class="container">
-      <h1 class="title">Lesson completed!</h1>
-      <h2 class="subtitle">
-        You've completed {rawChallenges.length} challenges
-      </h2>
-      <a class="button primary" href="{courseURL}">Continue to course page</a>
+      <div class="columns" in:scale>
+        <div class="column is-three-fifths">
+          <img src="images/mascot-jetpack.svg" alt="" />
+        </div>
+        <div class="column">
+          <h1 class="title is-2">Lesson completed!</h1>
+          <h2 class="subtitle">
+            You've completed {rawChallenges.length} challenges
+          </h2>
+          <a class="button primary is-medium" href="{courseURL}">
+            Continue to course page
+          </a>
+          <div class="bottom">
+            <h2 class="title is-5">Excited about LibreLingo?</h2>
+            <a
+              class="button secondary twitter"
+              target="_blank"
+              href="https://twitter.com/intent/tweet?hashtags=LibreLingo%2Copensource&url=https%3A%2F%2Flibrelingo.app&text=LibreLingo%20-%20an%20experiment%20to%20create%20a%20community-owned%20language%20learning%20tool%0A">
+              <Icon icon="twitter" prefix="fab" />
+              <div>Tweet about LibreLingo</div>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </section>
+
+<style>
+  .bottom {
+    position: absolute;
+    bottom: 0;
+  }
+  .twitter {
+    background-color: #55acee;
+    color: white;
+  }
+</style>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,6 +6,20 @@
   }
 </script>
 
+<svelte:head>
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta
+    name="twitter:description"
+    content="an experiment to create a community-owned language-learning
+    platform" />
+  <meta name="twitter:title" content="LibreLingo" />
+  <meta
+    name="twitter:image"
+    content="https://librelingo.app/images/social_cover_1.png" />
+  <meta name="twitter:creator" content="@cacophonism" />
+
+</svelte:head>
+
 {#if process.browser !== true}
   <div class="pageloader is-active">
     <span class="title">LibreLingo</span>
