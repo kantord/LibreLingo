43e4c1fe500366531160b015e75e16b63fe41647
Dániel Kántor <git@daniel-kantor.com>
2020-04-15 21:25:34 +0200

    refactor: create <Icon /> component

---
+++ src/components/Icon.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { library, dom } from "@fortawesome/fontawesome-svg-core"
+  import {
+    faVolumeUp,
+    faCheckSquare,
+    faDumbbell
+  } from "@fortawesome/free-solid-svg-icons"
+  library.add(faVolumeUp)
+  library.add(faCheckSquare)
+  library.add(faDumbbell)
+  dom.watch()
+
+  export let size = "medium"
+  export let icon
+</script>
+
+<span class="{`icon fa-${size}`}">
+  <i class="{`fas fa-${icon}`}"></i>
+</span>
