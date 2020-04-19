ce743fe8b2e44cfa98db88102b81de2c5048a809
Dániel Kántor <git@daniel-kantor.com>
2020-04-19 13:44:12 +0200

    chore: make visual regression tests more reliable

--- cypress/integration/common/everything_looks_correct.js
+++ cypress/integration/common/everything_looks_correct.js
@@ -1,5 +1,6 @@
 import { Then } from "cypress-cucumber-preprocessor/steps"
 
 Then("{} looks correct", snapshotsName => {
+    cy.get(".fontawesome-i2svg-pending").should("not.exist")
     cy.percySnapshot(snapshotsName)
 })
