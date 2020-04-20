7e12c253d0372a7d5d0ff27fe0404c6c92808e9e
Dániel Kántor <git@daniel-kantor.com>
2020-04-20 23:02:33 +0200

    chore: add util functions for spell checker

--- course/test_utils.py
+++ course/test_utils.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from .utils import clean_word
+from .utils import clean_word, validate_spelling
+from django.core.exceptions import ValidationError
 
 
 class CleanWordTestCase(TestCase):
@@ -17,3 +18,19 @@ class CleanWordTestCase(TestCase):
 
     def test_messy_word(self):
         self.assertEqual(clean_word(":::col·lecciona'm::?"), "col·lecciona'm")
+
+
+class SpellCheckValidatorTests(TestCase):
+    def test_validate_spelling_incorrect_1(self):
+        with self.assertRaises(ValidationError):
+            validate_spelling("thare is correct", "en")
+
+    def test_validate_spelling_incorrect_2(self):
+        with self.assertRaises(ValidationError):
+            validate_spelling("La manzana es pequna", "es")
+
+    def test_validate_spelling_correct_1(self):
+        validate_spelling("that is correct", "en")
+
+    def test_validate_spelling_correct_1(self):
+        validate_spelling("La manzana es pequeña", "es")
