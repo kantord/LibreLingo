46a45c4acd77c834e47bfa5b69eca34077458677
Dániel Kántor <git@daniel-kantor.com>
2020-05-01 16:35:43 +0200

    refactor(twitter button): implement using <Button />

--- src/components/ui-kit/Button.svelte
+++ src/components/ui-kit/Button.svelte
@@ -4,18 +4,28 @@
   export let inverted = false
   export let outlined = false
   export let hidden = false
+  export let color = "initial"
+  export let textColor = "initial"
+  export let target = "_self"
 </script>
 
 <a
+  style="--color:{color}; --textColor:{textColor};"
   class="button"
   class:is-primary="{primary}"
   class:is-inverted="{inverted}"
   class:is-outlined="{outlined}"
   class:is-hidden="{hidden}"
-  {href}>
+  {href}
+  {target}>
   <slot />
 </a>
 
 <style>
   @import "../../variables";
+
+  .button {
+    background-color: var(--color);
+    color: var(--textColor);
+  }
 </style>
