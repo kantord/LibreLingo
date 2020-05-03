b368d3e82b93f6ec6dd6e2486f310a4c8a5372b8
Navan Chauhan <navanchauhan@gmail.com>
2020-05-03 19:03:20 +0530

    Update _error.svelte

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,4 +1,9 @@
 <script>
+  import NavBar from "../components/NavBar"
+  import Mascot from "../components/Mascot-error"
+  import LicenseLogo from "../components/LicenseLogo"
+  import Button from "../components/ui-kit/Button"
+  import { _ } from "svelte-i18n"
   export let status
   export let error
 
@@ -6,36 +11,71 @@
 </script>
 
 <svelte:head>
-  <title>{status}</title>
+  <title>Error {status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
 
-<p>{error.message}</p>
+<section class="hero is-primary is-bold is-fullheight">
+  <div class="hero-head">
+    <NavBar />
+  </div>
+  <div class="hero-body">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-one-quarter">
+          <div class="mascot">
+            <Mascot shadow="{false}" glow="{true}" />
+          </div>
+        </div>
+        <div class="column">
+          <h1 class="title">
+            <img src="images/logo.svg" alt="LibreLingo" />
+          </h1>
+          <h2 class="subtitle">Oops! Error {status}</h2>
+          <p>Status: {error.message}</p>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+<div class="license">
+  <LicenseLogo />
+</div>
 
 {#if dev && error.stack}
   <pre>{error.stack}</pre>
 {/if}
 
 <style>
-  h1,
-  p {
-    margin: 0 auto;
+  @import "../variables";
+
+  .title img {
+    height: 3em;
   }
 
-  h1 {
-    font-size: 2.8em;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
+  .hero {
+    background-image: url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11 18c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm48 25c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm-43-7c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm63 31c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM34 90c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm56-76c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM12 86c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm28-65c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm23-11c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-6 60c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm29 22c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zM32 63c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm57-13c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-9-21c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM60 91c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM35 41c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM12 60c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z' fill='%2341809e' fill-opacity='0.1' fill-rule='evenodd'/%3E%3C/svg%3E"),
+      linear-gradient(
+        141deg,
+        $blue 0%,
+        $blue 40%,
+        adjust-color($blue, $red: -20%) 81%,
+        adjust-color($blue, $red: +40%) 100%
+      ) !important;
   }
 
-  p {
-    margin: 1em auto;
+  .license {
+    position: absolute;
+    right: 1em;
+    bottom: 1em;
   }
 
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
+  @include until($tablet) {
+    .mascot {
+      width: 45%;
+      margin: auto;
+      margin-top: -3em;
     }
   }
 </style>
