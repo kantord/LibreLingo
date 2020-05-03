bbc96b4072b677328fba5af2d47bb3b3ad36f586
Dániel Kántor <git@daniel-kantor.com>
2020-05-04 00:17:41 +0200

    refactor(challenge panel): use <Button /> component

--- src/components/ui-kit/Button.svelte
+++ src/components/ui-kit/Button.svelte
@@ -1,14 +1,20 @@
 <script>
+  import { createEventDispatcher } from "svelte"
+
+  const dispatch = createEventDispatcher()
   export let href
   export let primary = false
   export let info = false
   export let inverted = false
   export let outlined = false
   export let hidden = false
-  export let color = "initial"
-  export let textColor = "initial"
+  export let color = null
+  export let textColor = null
+  export let customColor = color != null
+  export let customTextColor = textColor != null
   export let target = "_self"
   export let size = "default"
+  export let type = null
 
   let styleTokens = `
     --color:${color};
@@ -16,18 +22,41 @@
   `
 </script>
 
-<a
-  style="{styleTokens}"
-  class="button is-{size}"
-  class:is-primary="{primary}"
-  class:is-info="{info}"
-  class:is-inverted="{inverted}"
-  class:is-outlined="{outlined}"
-  class:is-hidden="{hidden}"
-  {href}
-  {target}>
-  <slot />
-</a>
+{#if href}
+
+  <a
+    style="{styleTokens}"
+    class="button is-{size}"
+    class:is-primary="{primary}"
+    class:is-info="{info}"
+    class:is-inverted="{inverted}"
+    class:is-outlined="{outlined}"
+    class:is-hidden="{hidden}"
+    class:customColor
+    class:customTextColor
+    {href}
+    {target}>
+    <slot />
+  </a>
+{/if}
+
+{#if type}
+  <button
+    style="{styleTokens}"
+    class="button is-{size}"
+    class:is-primary="{primary}"
+    class:is-info="{info}"
+    class:is-inverted="{inverted}"
+    class:is-outlined="{outlined}"
+    class:is-hidden="{hidden}"
+    class:customColor
+    class:customTextColor
+    on:click="{() => dispatch('click')}"
+    {type}>
+    <slot />
+  </button>
+
+{/if}
 
 <style>
   @import "../../variables";
@@ -37,8 +66,11 @@
   $button-hover-color: $link-hover !default;
   $button-focus-color: $link-focus !default;
 
-  .button {
+  .button.customColor {
     background-color: var(--color);
+  }
+
+  .button.customTextColor {
     color: var(--textColor);
   }
 </style>
