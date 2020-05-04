94b68c712bbd7c081808ba52fcf53a6625f75665
Dániel Kántor <gitlab@daniel-kantor.com>
2020-05-04 23:07:21 +0000

    Updates

---
+++ client/3a025847f69c40aa4735/41.41.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[41],{280:function(a){a.exports=JSON.parse('{"id":"434d43b3","challenges":[{"id":662,"type":"shortInput","phrase":[{"word":"water"}],"formInTargetLanguage":["agua","el agua"]},{"id":661,"type":"shortInput","phrase":[{"word":"water"}],"formInTargetLanguage":["agua","el agua"]}]}')}}]);
