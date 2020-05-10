985edf2c6e4b429cc63b7906ff79a6fa15b60c0f
Dániel Kántor <git@daniel-kantor.com>
2020-05-10 20:48:59 +0200

    test(savepractice): add missing test

--- src/db/db.js
+++ src/db/db.js
@@ -6,4 +6,18 @@ if (process.browser === true) {
     window._DB = db
 }
 
+if (process.env.JEST_WORKER_ID !== undefined) {
+    // This is a test database for Jest tests that can reset itself
+    const PouchDB = require("pouchdb")
+    db = new PouchDB("localData")
+    db.__reset = async () => {
+        const allDocs = await db.allDocs()
+        await Promise.all(
+            allDocs.rows.map(function(row) {
+                return db.remove(row.id, row.value.rev)
+            })
+        )
+    }
+}
+
 export default db
