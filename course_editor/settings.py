018f2dba72818cf3cedfe9c3f2eecb087e15a8fe
Dániel Kántor <git@daniel-kantor.com>
2020-05-23 22:00:57 +0200

    fix(course editor): fix django static dir

--- course_editor/settings.py
+++ course_editor/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'workspaces', 'web', 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
