308e858f5d2faf4b0c55432b56727c6b886e7546
Dániel Kántor <git@daniel-kantor.com>
2020-05-30 13:41:48 +0200

    feat: add login function

---
+++ workspaces/web/src/db/getUserDbName.js
@@ -0,0 +1,7 @@
+const hashUsername = (username) =>
+    username
+        .split("")
+        .map((c) => c.charCodeAt(0).toString(16))
+        .join("")
+
+export default (username) => `userdb-${hashUsername(username)}`
