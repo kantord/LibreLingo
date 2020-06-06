7db53babe87812c684644dc7550a6be68047446d
Dániel Kántor <git@daniel-kantor.com>
2020-06-06 23:43:46 +0200

    feat: add log in form

--- workspaces/web/cypress/integration/common/i_click.js
+++ workspaces/web/cypress/integration/common/i_click.js
@@ -3,3 +3,7 @@ import { Then } from "cypress-cucumber-preprocessor/steps"
 Then("I click {string}", (text) => {
     cy.contains(text).click()
 })
+
+Then("I click the {string} button", (text) => {
+    cy.get("button").contains(text).click()
+})
