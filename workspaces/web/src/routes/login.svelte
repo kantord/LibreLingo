d5ebe2a4666a6b1e1b75abe3235ecae571bc3c8f
Dániel Kántor <git@daniel-kantor.com>
2020-06-06 23:43:46 +0200

    feat: add log in form

---
+++ workspaces/web/src/routes/login.svelte
@@ -0,0 +1,71 @@
+<script>
+  import db from "../db/db.js"
+  import { onMount } from "svelte"
+  import hotkeys from "hotkeys-js"
+  import NavBar from "../components/NavBar.svelte"
+  import Icon from "../components/Icon.svelte"
+  import Button from "lluis/Button"
+
+  let error = null
+  let username = ""
+  let password = ""
+
+  const handleLogin = async () => {
+    try {
+      await window._Login(username, password)
+    } catch (e) {
+      error = e
+    }
+  }
+
+  /*onMount(() => {*/
+  /*hotkeys.unbind("enter")*/
+  /*hotkeys("enter", () => {*/
+  /*handleLogin()*/
+  /*})*/
+  /*})*/
+</script>
+
+<NavBar dark />
+
+<section class="section">
+
+  <div class="container">
+
+    <form on:submit|preventDefault="{handleLogin}">
+      <h2 class="is-size-2">Log in</h2>
+
+      <div class="field">
+        <label class="label">Username</label>
+        <div class="control has-icons-left">
+          <input
+            class="input"
+            type="text"
+            name="username"
+            bind:value="{username}" />
+          <Icon size="small" icon="user" left />
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Password</label>
+        <p class="control has-icons-left">
+          <input
+            class="input"
+            type="password"
+            name="password"
+            bind:value="{password}" />
+          <Icon size="small" icon="lock" left />
+        </p>
+      </div>
+
+      {#if error !== null}
+        <p class="help is-danger">{error}</p>
+      {/if}
+
+      <Button on:click="{handleLogin}">Log in</Button>
+      <input type="submit" style="visibility: hidden;" />
+    </form>
+  </div>
+
+</section>
