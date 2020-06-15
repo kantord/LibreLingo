d01fff4f6b54edfbf4869abe71aaa5a880a4d998
Dániel Kántor <git@daniel-kantor.com>
2020-06-15 22:04:21 +0200

    feat: customize things to match theme

--- workspaces/web/src/routes/development.svelte
+++ workspaces/web/src/routes/development.svelte
@@ -9,21 +9,21 @@
 
   import shuffle from "lodash.shuffle"
 
-  const API_URL = "https://api.github.com/repos/kantord/LibreLingo/issues"
+  const API_URL =
+    "https://api.github.com/repos/kantord/LibreLingo/issues?per_page=100"
   let issues = null
 
   if (process.browser === true) {
     fetch(API_URL)
       .then((res) => res.json())
-      .then(
-        (res) =>
-          (issues = shuffle(res)
-            .filter(
-              ({ state, pull_request, labels }) =>
-                state === "open" && !pull_request && labels.length !== 0
-            )
-            .slice(10))
-      )
+      .then((res) => {
+        issues = shuffle(res)
+          .filter(
+            ({ state, pull_request, labels }) =>
+              state === "open" && !pull_request && labels.length !== 0
+          )
+          .slice(0, 10)
+      })
       .catch(() => (issues = []))
   }
 </script>
@@ -38,7 +38,7 @@
   </div>
 {/if}
 
-<section class="hero is-primary is-bold">
+<section class="hero is-primary">
   <div class="hero-head">
     <NavBar />
   </div>
@@ -173,45 +173,6 @@
         </progress>
       </div>
 
-      {#if issues !== null && issues.length}
-        <div class="container">
-          <h3 class="is-size-3">
-            Looking for a challange? Check out these issues:
-          </h3>
-          <div class="columns is-multiline">
-            {#each issues as { title, html_url, labels }}
-              <div class="column is-one-third">
-                <a href="{html_url}">
-                  <div class="card">
-                    <header class="card-header">
-                      <p class="card-header-title">{title}</p>
-                    </header>
-                    <div class="card-content">
-                      <div class="content">
-                        <div class="tags">
-                          {#each labels as { name, color }}
-                            <div
-                              class="tag"
-                              style="{`background-color: #${color}`}">
-                              {name}
-                            </div>
-                          {/each}
-                        </div>
-                      </div>
-                    </div>
-                    <footer class="card-footer">
-                      <div class="card-footer-item">
-                        Contribute to this issue
-                      </div>
-                    </footer>
-                  </div>
-                </a>
-              </div>
-            {/each}
-          </div>
-        </div>
-      {/if}
-
       <div class="buttons buttons-bottom">
         <a
           class="button is-primary is-inverted is-outlined"
@@ -227,8 +188,51 @@
   </div>
 </section>
 
+{#if issues !== null && issues.length}
+  <section class="section">
+    <div class="container">
+      <h3 class="is-size-3">
+        Looking for a challange? Check out these issues:
+      </h3>
+      <div class="columns is-multiline">
+        {#each issues as { title, html_url, labels }}
+          <div class="column is-one-third">
+            <a href="{html_url}">
+              <div class="card">
+                <header class="card-header">
+                  <p class="card-header-title">{title}</p>
+                </header>
+                <div class="card-content">
+                  <div class="content">
+                    <div class="tags">
+                      {#each labels as { name, color }}
+                        <div
+                          class="tag"
+                          style="{`background-color: #${color}`}">
+                          {name}
+                        </div>
+                      {/each}
+                    </div>
+                  </div>
+                </div>
+                <footer class="card-footer">
+                  <div class="card-footer-item">Contribute to this issue</div>
+                </footer>
+              </div>
+            </a>
+          </div>
+        {/each}
+      </div>
+    </div>
+  </section>
+{/if}
+
 <style>
   @import "../variables";
+
+  .card {
+    background: white;
+  }
 
   .tag {
     color: $white;
@@ -276,17 +280,6 @@
       text-decoration: underline;
       text-decoration-style: dotted;
     }
-  }
-
-  .hero {
-    background-image: url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11 18c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm48 25c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm-43-7c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm63 31c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM34 90c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm56-76c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM12 86c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm28-65c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm23-11c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-6 60c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm29 22c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zM32 63c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm57-13c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-9-21c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM60 91c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM35 41c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM12 60c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z' fill='%2341809e' fill-opacity='0.1' fill-rule='evenodd'/%3E%3C/svg%3E"),
-      linear-gradient(
-        141deg,
-        $blue 0%,
-        $blue 40%,
-        adjust-color($blue, $red: -20%) 81%,
-        adjust-color($blue, $red: +40%) 100%
-      ) !important;
   }
 
   @include until($tablet) {
