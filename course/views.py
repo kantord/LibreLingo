c17697ad0db11d020e5f66f8b5faa0a2ee03afca
Dániel Kántor <git@daniel-kantor.com>
2020-06-26 21:10:17 +0200

    feat: add backend for registration

--- course/views.py
+++ course/views.py
@@ -1,9 +1,11 @@
+import requests
 import datetime
 import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from django import forms
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 
 
 class UserForm(forms.Form):
@@ -26,7 +28,7 @@ def error(error_code, details=None):
 def fail_with(failures):
     return respond({
         "success": False,
-        "errors": failures
+        "error": failures
     })
 
 
@@ -45,7 +47,22 @@ def register(request):
         errors = {**{field.name: field.errors for field in form},
                   **{"_form": form.non_field_errors()}}
         return fail_with(error("invalid-payload", errors))
-    now = datetime.datetime.now()
-    return respond({
-        "success": True,
-    })
+    data = form.cleaned_data
+    couch_payload = {"name": data["username"], "password": data["password"], "email": data["email"], "created": datetime.datetime.now().isoformat(),
+                     "roles": [], "type": "user"}
+    try:
+        headers = {'Content-Type': "application/json",
+                   'Accept': "application/json"}
+        response = requests.put(
+            'https://sync.librelingo.app/_users/org.couchdb.user:' + data["username"], json=couch_payload, headers=headers, auth=(settings.COUCH_USER, settings.COUCH_PASS))
+        response_data = response.json()
+        if response.status_code == 201:
+            return respond({
+                "success": True,
+            })
+        else:
+            if response_data["error"] == "conflict":
+                return fail_with(error("invalid_payload", {"username": ["This username is taken"], "password": [], "email": [], "_form": []}))
+            return fail_with(error("database-error", response_data))
+    except Exception as server_error:
+        return fail_with(error("server-error", str(server_error)))
