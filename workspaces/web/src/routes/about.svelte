0f7d2404be4d08f9c0207aced0818be64edbb7bd
Dániel Kántor <git@daniel-kantor.com>
2020-07-12 18:22:26 +0200

    refactor: make it easier to create markdown based pages

--- workspaces/web/src/routes/about.svelte
+++ workspaces/web/src/routes/about.svelte
@@ -1,49 +1,17 @@
 <script context="module">
+  import MarkDownPage, { getMarkDownData } from "../components/MarkDownPage"
+
   export async function preload(page, session) {
-    const remark = await require("remark")
-    const markdown = await import("remark-parse")
-    const html = await import("rehype-stringify")
-    const readme = await import("../../../../README.md")
-    const remark2rehype = await import("remark-rehype")
-    const format = await import("rehype-format")
-
-    const readmeHTML = await remark()
-      .use(markdown.default, { gfm: true, commonmark: true })
-      .use(remark2rehype.default)
-      .use(format.default)
-      .use(html.default)
-      .process(readme.default)
-
     return {
-      readmeHTML: readmeHTML.contents.split("<h2>Tech stack</h2>")[0],
+      readmeHTML: (
+        await getMarkDownData(await import("../../../../README.md"))
+      ).contents.split("<h2>Tech stack</h2>")[0],
     }
   }
 </script>
 
 <script>
-  import NavBar from "../components/NavBar"
-  import Button from "lluis/Button"
-
   export let readmeHTML
 </script>
 
-<svelte:head>
-  <title>LibreLingo - about LibreLingo</title>
-</svelte:head>
-
-<NavBar dark />
-
-<section class="hero is-primary">
-  <div class="hero-body">
-    <div class="container">
-      <h1 class="title">About LibreLingo</h1>
-      <div class="section content box">
-        {@html readmeHTML}
-        <hr />
-        <Button primary href="https://github.com/kantord/LibreLingo">
-          Continue reading on GitHub
-        </Button>
-      </div>
-    </div>
-  </div>
-</section>
+<MarkDownPage {readmeHTML} />
