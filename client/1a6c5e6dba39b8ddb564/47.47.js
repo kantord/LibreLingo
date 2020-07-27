65652dc01dadb571f351fee57636063fb105b8ec
Dániel Kántor <gitlab@daniel-kantor.com>
2020-07-27 13:34:53 +0000

    Updates

---
+++ client/1a6c5e6dba39b8ddb564/47.47.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[47],{386:function(e){e.exports=JSON.parse('{"id":"434d43b3","challenges":[{"id":6663,"type":"listeningExercise","answer":"perro","audio":"perro","meaning":"foo bar baz"}]}')}}]);
---
+++ client/1a6c5e6dba39b8ddb564/52.52.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{391:function(e){e.exports=JSON.parse('{"id":"434d43b3","challenges":[{"id":663,"type":"shortInput","phrase":[{"word":"foo"}],"formInTargetLanguage":["leche","la leche"]}]}')}}]);
