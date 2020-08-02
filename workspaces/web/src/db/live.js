856d3952f749053be11dbb31ffd28b9d86c762f3
Dániel Kántor <git@daniel-kantor.com>
2020-08-02 15:29:13 +0200

    fix: fix database subscriptions

--- workspaces/web/src/db/live.js
+++ workspaces/web/src/db/live.js
@@ -1,13 +1,7 @@
-import db from "./db"
-
 export default (listener) => {
     listener()
-    db &&
-    db
-        .changes({
-            since: "now",
-            live: true,
-            include_docs: true,
-        })
-        .on("change", listener)
+    if (process.browser === true && process.env.JEST_WORKER_ID === undefined) {
+        const authStore = require("../auth").default
+        authStore.subscribe(listener)
+    }
 }
