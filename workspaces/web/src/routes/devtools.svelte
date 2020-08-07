6615c5688dbb625d64abee80e8e1eb6e9d772099
Dániel Kántor <git@daniel-kantor.com>
2020-08-07 11:32:02 +0200

    chore: create a devtools page

--- workspaces/web/src/routes/devtools.svelte
+++ workspaces/web/src/routes/devtools.svelte
@@ -1,3 +1,19 @@
+<script context="module">
+  export async function preload(page, session) {
+    if (!process.browser) {
+      const fs = require("fs")
+      const util = require("util")
+      const readdir = util.promisify(fs.readdir)
+
+      return {
+        testSkills: (await readdir("./src/courses/test/challenges")).map(
+          (fname) => fname.split(".")[0]
+        ),
+      }
+    }
+  }
+</script>
+
 <script>
   import NavBar from "../components/NavBar"
   import Mascot from "../components/Mascot"
@@ -5,6 +21,8 @@
   import GitHubButton from "../components/GitHubButton"
   import Column from "lluis/Column"
   import Columns from "lluis/Columns"
+
+  export let testSkills = []
 </script>
 
 <svelte:head>
@@ -15,7 +33,22 @@
 
 <section class="section">
   <div class="container">
-    <h1 class="is-size-1">Development tools</h1>
-    <h2 class="is-size-2">Test pages</h2>
+    <div class="content">
+      <h1 class="is-size-1">Development tools</h1>
+      <h2 class="is-size-2">Test pages</h2>
+      <ul>
+        <li>
+          <a href="/course/test">Test course</a>
+        </li>
+        {#each testSkills as testSkill}
+          <li>
+            <a target="_blank" href="/course/test/skill/{testSkill}">
+              Test skill:
+              <b>{testSkill}</b>
+            </a>
+          </li>
+        {/each}
+      </ul>
+    </div>
   </div>
 </section>
