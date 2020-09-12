fc56eedc844083f0713abc241bdbec9baf509622
Dániel Kántor <git@daniel-kantor.com>
2020-09-12 11:30:19 +0200

    refactor(skillcard): add <ImageSet />

---
+++ workspaces/web/src/components/SkillCard/ImageSet.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let images
+</script>
+
+<figure class="image image-set is-96x96">
+  <img src="{images[0]}" alt="" />
+  <img src="{images[1]}" alt="" />
+  <img src="{images[2]}" alt="" />
+</figure>
+
+<style>
+  .image-set {
+    position: relative;
+    overflow: hidden;
+  }
+
+  .image-set img {
+    left: 15%;
+    top: 15%;
+    width: 70%;
+    position: absolute;
+  }
+
+  .image-set img:first-child {
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+
+  .image-set img:last-child {
+    position: absolute;
+    left: 30%;
+    top: 30%;
+  }
+</style>
