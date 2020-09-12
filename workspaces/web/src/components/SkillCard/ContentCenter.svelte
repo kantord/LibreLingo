c45d3dd0bf5f1c1bec20c7e66a8cdaae0b77262b
Dániel Kántor <git@daniel-kantor.com>
2020-09-12 12:16:56 +0200

    refactor(skillcard): create <ContentCenter />

---
+++ workspaces/web/src/components/SkillCard/ContentCenter.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Summary from "./Summary"
+
+  export let title
+  export let completed
+  export let started
+  export let summary
+  export let progress
+  export let levels
+  export let stale
+</script>
+
+<div class="media-content" class:completed class:stale>
+  <p class="title is-4">{title}</p>
+  {#if completed || !started}
+    <Summary summary="{summary}" stale="{stale}" completed="{completed}" />
+  {/if}
+  {#if !completed && started}
+    <progress class="progress" value="{progress}" max="{levels}"></progress>
+  {/if}
+</div>
+
+<style>
+  @import "../../variables";
+
+  .completed,
+  .stale {
+    * {
+      color: $white;
+    }
+  }
+</style>
