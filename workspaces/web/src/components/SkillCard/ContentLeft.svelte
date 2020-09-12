afa66d60a1ca65bcd966b0d85749148bb3c98006
Dániel Kántor <git@daniel-kantor.com>
2020-09-12 11:37:27 +0200

    refactor(skillcard): add <ContentLeft />

---
+++ workspaces/web/src/components/SkillCard/ContentLeft.svelte
@@ -0,0 +1,34 @@
+<script>
+  import ImageSet from "./ImageSet"
+
+  export let imageSet
+  export let stale
+  export let completed
+</script>
+
+{#if imageSet && imageSet.length}
+  <div class="media-left" class:stale class:completed>
+    <div class="image-set">
+      <ImageSet
+        images="{[`images/${imageSet[0]}_tinier.jpg`, `images/${imageSet[1]}_tinier.jpg`, `images/${imageSet[2]}_tiny.jpg`]}" />
+    </div>
+  </div>
+{/if}
+
+<style>
+  @import "../../variables";
+
+  .stale,
+  .completed {
+    mix-blend-mode: screen;
+
+    .image-set {
+      filter: saturate(0);
+
+      img {
+        box-sizing: border-box;
+        border: 1px solid rgba($white, 0.3);
+      }
+    }
+  }
+</style>
