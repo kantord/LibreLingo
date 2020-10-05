381ae85815dd4466042c886d7e7e2660dd3951ae
Dávid <luterancik@bart.sk>
2020-10-05 17:58:35 +0200

    feat(sounds): reverting sound const to avoid extra latency, added new line at the end of file

--- workspaces/web/src/media/sound.js
+++ workspaces/web/src/media/sound.js
@@ -1,9 +1,15 @@
 import { Howl } from "howler"
 
 const sound = {
-    correct: () => playAudio("sound","correct"),
-    wrong: () => playAudio("sound","wrong"),
-    fanfare: () => playAudio("sound","fanfare")
+    correct: new Howl({
+        src: ["sound/correct.mp3"],
+    }),
+    wrong: new Howl({
+        src: ["sound/wrong.mp3"],
+    }),
+    fanfare: new Howl({
+        src: ["sound/fanfare.mp3"],
+    }),
 }
 
 export const playAudio = (type,filename) => {
