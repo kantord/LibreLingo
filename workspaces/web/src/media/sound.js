870723d9e63a937dea516b98b525019b6023a4f5
Dávid <luterancik@bart.sk>
2020-10-05 23:25:52 +0200

    feat(sounds): disabling the howler for cypress tests

--- workspaces/web/src/media/sound.js
+++ workspaces/web/src/media/sound.js
@@ -1,23 +1,33 @@
-import { Howl } from "howler"
+import { Howl , Howler } from "howler"
+
+const disableHowlerForCypress = () => {
+    if (Howler.volume() !== 0 && typeof window !== 'undefined' && window.Cypress) {
+        Howler.volume(0);
+        console.log('Sounds while testing with cypress are disabled')
+    }
+}
 
 const sound = {
     correct: new Howl({
         src: ["sound/correct.mp3"],
+        onload: disableHowlerForCypress
     }),
     wrong: new Howl({
         src: ["sound/wrong.mp3"],
+        onload: disableHowlerForCypress
     }),
     fanfare: new Howl({
         src: ["sound/fanfare.mp3"],
+        onload: disableHowlerForCypress
     }),
 }
 
 export const playAudio = (type,filename) => {
-    if(!window.Cypress){
-        new Howl({
-            src: [`${type}/${filename}.mp3`],
-        }).play()
-    }
+    disableHowlerForCypress
+    
+    new Howl({
+        src: [`${type}/${filename}.mp3`],
+    }).play()
 }
 
 export default sound
