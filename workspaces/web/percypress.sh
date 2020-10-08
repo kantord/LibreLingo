baee8b220594cf9712c0cbb94ad64f72ca04e888
Artur Bauer <bauerart1@gmail.com>
2020-10-08 21:42:39 +0200

    ci(percypress.sh): fix cypress tests on forks

--- workspaces/web/percypress.sh
+++ workspaces/web/percypress.sh
@@ -3,5 +3,7 @@ if [ "$KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC" = "" ]; then
   KNAPSACK_PRO_ENDPOINT=https://api-disabled-for-fork.knapsackpro.com \
     KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC=disabled-for-fork \
     KNAPSACK_PRO_MAX_REQUEST_RETRIES=0 
+    yarn cy:run
+else
+  KNAPSACK_PRO_TEST_FILE_PATTERN=\"cypress/integration/**/*.{feature,features}\" percy exec -- $(npm bin)/knapsack-pro-cypress
 fi
-KNAPSACK_PRO_TEST_FILE_PATTERN=\"cypress/integration/**/*.{feature,features}\" percy exec -- $(npm bin)/knapsack-pro-cypress
