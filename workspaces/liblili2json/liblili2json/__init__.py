90fd2bdcf06bef36c42f9fd0faa06c985ebbe06e
Dániel Kántor <git@daniel-kantor.com>
2020-10-08 23:06:33 +0200

    chore: set up boilerplate for liblili_to_json

---
+++ workspaces/liblili2json/liblili2json/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.1.0'
+
+
+def get_course_data():
+    return {
+        "languageName": "my language",
+        "languageCode": "de",
+        "specialCharacters": ["ä", "ß"]
+    }
