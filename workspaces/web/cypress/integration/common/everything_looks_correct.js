349c6b5a614d16a42fe0320fc7f6fde524c5055f
Dániel Kántor <git@daniel-kantor.com>
2020-10-12 15:11:40 +0200

    feat: generate screenshots automatically for translation keys

--- workspaces/web/cypress/integration/common/everything_looks_correct.js
+++ workspaces/web/cypress/integration/common/everything_looks_correct.js
@@ -4,4 +4,27 @@ Then("{} looks correct", (snapshotsName) => {
     cy.get(".fontawesome-i2svg-pending").should("not.exist")
     cy.document().its("fonts.status").should("equal", "loaded")
     cy.percySnapshot(snapshotsName)
+
+		cy.get('[data-tkey]').each($el => {
+			const tkey = $el.data('tkey')
+			const { left, top, width, height } = $el[0].getBoundingClientRect()
+			const highlight = document.createElement("div"); 
+			highlight.setAttribute("style", `
+				position: fixed;
+				left: ${left - 5}px;
+				top: ${top - 5}px;
+				width: ${width + 15}px;
+				height: ${height + 15}px;
+				border: 3px dashed red;
+				z-index: 1000000;
+			`);
+			cy.window().then((win) => {
+				win.document.body.appendChild(highlight)
+			})
+			console.log(`📸 Creating screenshots for ${tkey}}`)
+			cy.screenshot(`__tkey-${tkey}`)
+			cy.window().then((win) => {
+				win.document.body.removeChild(highlight)
+			})
+		})
 })
