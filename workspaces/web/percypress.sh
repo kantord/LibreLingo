ea19bbc036a88074af38e2e450198d6969713e8d
Dániel Kántor <git@daniel-kantor.com>
2020-10-15 21:48:21 +0200

    fix: fix percy in forks

--- workspaces/web/percypress.sh
+++ workspaces/web/percypress.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 if [ "$KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC" = "" ]; then
+  echo "KNAPSACK PRO NOT ENABLED"
   KNAPSACK_PRO_ENDPOINT=https://api-disabled-for-fork.knapsackpro.com \
     KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC=disabled-for-fork \
     KNAPSACK_PRO_MAX_REQUEST_RETRIES=0 
-    yarn cy:run
+    percy exec -- yarn cy:run
 else
+  echo "KNAPSACK PRO ENABLED"
   KNAPSACK_PRO_TEST_FILE_PATTERN=\"cypress/integration/**/*.{feature,features}\" percy exec -- $(npm bin)/knapsack-pro-cypress
 fi
