9b6578d90b7fb30899d811c371a7a4fd6088a663
Tassatir <gokaymrc@hotmail.com>
2020-10-16 23:37:40 +0300

    feat(navbar): added sponsor button

---
+++ workspaces/web/src/components/SponsorButton.svelte
@@ -0,0 +1,9 @@
+<script lang="typescript">
+  import Button from "lluis/Button"
+  import Icon from "lluis/Icon"
+</script>
+
+<Button href="http://github.com/sponsors/kantord" size="small" outlined inverted info>
+<Icon icon="heart"></Icon>
+<div>Sponsor</div>
+</Button>
