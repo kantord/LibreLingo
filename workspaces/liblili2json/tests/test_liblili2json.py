636eada0ec7aebf4bfb9e95818c3e338ca5633df
Dániel Kántor <git@daniel-kantor.com>
2020-10-21 22:00:06 +0200

    feat(liblili2json): export module titles

--- workspaces/liblili2json/tests/test_liblili2json.py
+++ workspaces/liblili2json/tests/test_liblili2json.py
@@ -1,16 +1,24 @@
 from liblili2json import get_course_data
 from liblili2json import Course
+from liblili2json import Module
 
 fakeCourse1 = Course(
     language_name="my language",
     language_code="de",
-    special_characters=["ä", "ß"]
+    special_characters=["ä", "ß"],
+    modules=[
+        Module(title="Basics", skills=[]),
+        Module(title="Phrases", skills=[]),
+    ]
 )
 
 fakeCourse2 = Course(
     language_name="another language",
     language_code="tr",
-    special_characters=["ç", "ş"]
+    special_characters=["ç", "ş"],
+    modules=[
+        Module(title="Animals", skills=[]),
+    ]
 )
 
 
@@ -18,7 +26,15 @@ def test_get_course_data_return_value():
     assert get_course_data(fakeCourse1) == {
         "languageName": "my language",
         "languageCode": "de",
-        "specialCharacters": ["ä", "ß"]
+        "specialCharacters": ["ä", "ß"],
+        "modules": [
+            {
+                "title": "Basics",
+            },
+            {
+                "title": "Phrases",
+            }
+        ]
     }
 
 
@@ -26,5 +42,10 @@ def test_get_course_data_return_value_2():
     assert get_course_data(fakeCourse2) == {
         "languageName": "another language",
         "languageCode": "tr",
-        "specialCharacters": ["ç", "ş"]
+        "specialCharacters": ["ç", "ş"],
+        "modules": [
+            {
+                "title": "Animals",
+            },
+        ]
     }
