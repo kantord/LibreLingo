6e87bf25010701fca029ac7f8d1ae98fd4fa2abd
Christian Dimas <christian.dimas.94@gmail.com>
2020-10-28 23:17:37 +0700

    feat: implement title component

---
+++ workspaces/lluis/Title.svelte
@@ -0,0 +1,16 @@
+<script lang="typescript">
+  export let sizeMobile = null
+  export let sizeTablet = null
+  export let sizeDesktop = null
+  export let isSubtitle = false
+  export let isSpaced = false
+  export let size = isSubtitle ? 5 : 3
+</script>
+
+<h1
+  class:title={!isSubtitle}
+  class:subtitle={isSubtitle}
+  class="{`is-size-${size} is-size-${sizeMobile || size}-mobile is-size-${sizeTablet || size}-tablet is-size-${sizeDesktop || size}-desktop`}"
+  class:is-spaced={isSpaced}>
+  <slot />
+</h1>
