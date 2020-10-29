11dc9f5b6ed9f70d2e2709bdda13038e8f1f39cf
Christian Dimas <christian.dimas.94@gmail.com>
2020-10-29 10:45:12 +0700

    refactor: import title style to svelte component

--- workspaces/web/src/routes/dev-typography.svelte
+++ workspaces/web/src/routes/dev-typography.svelte
@@ -13,14 +13,14 @@
     <div class="tile is-ancestor">
       <div class="tile is-vertical is-8">
           <div class="tile is-parent is-vertical">
-            <h1 class="is-size-2 has-text-centered title">Fonts Visual Guide</h1>
+            <Title size={2} align="centered">Fonts Visual Guide</Title>
             <article class="tile is-child notification is-primary p-4">
-              <h1 class="is-size-3 has-text-weight-semibold">Types of headings</h1>
-              <h1 class="title">Title</h1>
-              <h2 class="subtitle">Subtitle</h2>
+              <Title size={3} textWeight="semibold">Types of headings</Title>
+              <Title>Title</Title>
+              <Title isSubtitle={true}>Subtitle</Title>
             </article>
             <article class="tile is-child notification is-primary p-4">
-              <h1 class="is-size-3 has-text-weight-semibold">Sizes</h1>
+              <Title size={3} textWeight="semibold">Sizes</Title>
               <Title size={1}>Title 1</Title>
               <Title size={2}>Title 2</Title>
               <Title size={3}>Title 3</Title>
