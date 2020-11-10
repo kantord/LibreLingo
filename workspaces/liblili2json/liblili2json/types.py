8816ac281b4792ba5acd9bf3ccccaf6591ae53ae
Dániel Kántor <git@daniel-kantor.com>
2020-11-10 09:27:35 +0100

    feat(liblili2json): add get_challenges_data

---
+++ workspaces/liblili2json/liblili2json/types.py
@@ -0,0 +1,38 @@
+from collections import namedtuple
+
+Course = namedtuple("Course", [
+    "language_name",
+    "language_code",
+    "special_characters",
+    "modules",
+    "license",
+])
+
+License = namedtuple("License", [
+    "name",
+    "full_name",
+    "link",
+])
+
+Module = namedtuple("Module", [
+    "title",
+    "skills",
+])
+
+Skill = namedtuple("Skill", [
+    "name",
+    "id",
+    "words",
+    "phrases",
+    "image_set"
+])
+
+Word = namedtuple("Word", [
+    "in_target_langauge",
+    "in_source_langauge",
+])
+
+Phrase = namedtuple("Phrase", [
+    "in_target_langauge",
+    "in_source_langauge",
+])
