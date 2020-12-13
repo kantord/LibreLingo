468d250e01da1758e4118cd9581006cf6f9e83ab
Dániel Kántor <git@daniel-kantor.com>
2020-12-13 21:52:53 +0100

    refactor(liblili2json): create get_dictionary_item()

--- workspaces/liblili2json/liblili2json/__init__.py
+++ workspaces/liblili2json/liblili2json/__init__.py
@@ -226,15 +226,21 @@ def define_words_in_sentence(course, sentence, reverse):
     return [define_word(course, word, reverse) for word in sentence.split()]
 
 
-def define_word(course, word, reverse):
+def get_dictionary_item(course, word, reverse):
     dictionary_item = list(
         filter(
             lambda item: item.word == word and item.reverse == reverse,
             course.dictionary))
+
+    return dictionary_item[0] if dictionary_item else None
+
+
+def define_word(course, word, reverse):
+    dictionary_item = get_dictionary_item(course, word, reverse)
     if dictionary_item:
         return {
             "word": word,
-            "definition": dictionary_item[0].definition
+            "definition": dictionary_item.definition
         }
 
     return {"word": word}
