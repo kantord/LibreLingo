77394bc5f26028aed61ce366b20a6b2c59cedd7c
Dániel Kántor <git@daniel-kantor.com>
2020-12-25 14:12:55 +0100

    docs(liblili2json): add docstring to clean_word

--- workspaces/liblili2json/liblili2json/utils.py
+++ workspaces/liblili2json/liblili2json/utils.py
@@ -2,6 +2,9 @@ import re
 
 
 def clean_word(word):
+    """
+        Remove punctuation and other special characters from a word.
+    """
     MATCH_NON_WORD_CHARACTERS_BEGINNING = re.compile("^[^\\w']+")
     MATCH_NON_WORD_CHARACTERS_END = re.compile("[^\\w']+$")
     return MATCH_NON_WORD_CHARACTERS_BEGINNING.sub(
