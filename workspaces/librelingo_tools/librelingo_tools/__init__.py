7cbe8602615bf56e525f4069c5e1425b4f43c9d7
Dániel Kántor <git@daniel-kantor.com>
2021-01-11 20:46:01 +0100

    docs(tools): document loading courses

--- workspaces/librelingo_tools/librelingo_tools/__init__.py
+++ workspaces/librelingo_tools/librelingo_tools/__init__.py
@@ -2,14 +2,8 @@
 Export LibreLingo courses in the JSON format expected by the web app
 """
 
-from .data_types import *
-from .utils import *
-from .dictionary import *
-from .challenge_types import *
-from .module import *
-from .course import *
-from .skills import *
-from .export import *
-from . import yaml_loader
+import librelingo_tools.data_types
+import librelingo_tools.export
+import librelingo_tools.yaml_loader
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
