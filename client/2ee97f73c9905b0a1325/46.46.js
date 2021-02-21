9b7a44b3b7d5a62429795b6344b2009d487891fa
Dániel Kántor <gitlab@daniel-kantor.com>
2021-02-21 10:57:28 +0000

    Updates

--- client/2ee97f73c9905b0a1325/46.46.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[46],{351:function(e){e.exports=JSON.parse('{"id":"434d43b3","levels":1,"challenges":[{"id":6663,"type":"shortInput","phrase":[{"word":"Dog"}],"pictures":["dog1.jpg","dog1.jpg","dog1.jpg"],"formInTargetLanguage":["perro","el perro","can"]}]}')}}]);
