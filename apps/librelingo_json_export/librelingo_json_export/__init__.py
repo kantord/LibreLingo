fadc1c30a240ad0363071fe06e054ebf90c9ebbc
Dániel Kántor <git@daniel-kantor.com>
2021-02-24 19:54:50 +0100

    feat: create librelingo_yaml_loader

--- apps/librelingo_json_export/librelingo_json_export/__init__.py
+++ apps/librelingo_json_export/librelingo_json_export/__init__.py
@@ -2,6 +2,6 @@
 Export LibreLingo courses in the JSON format used by the web app
 """
 
-from librelingo_json_export.cli import *
+import librelingo_json_export.export
 
-__version__ = '0.1.0'
+__version__ = '0.1.2'
