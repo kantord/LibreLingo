f8be7b945b8ec5e6981acd799e14f391fa00a4db
Dániel Kántor <git@daniel-kantor.com>
2021-03-02 20:26:47 +0100

    ci: verify that PyPi README files are up to date

---
+++ scripts/verifyPypiReadmeFiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./scripts/updatePypiReadmeFiles.sh
+
+if git diff-index --quiet HEAD --; then
+	echo "🎉 Amazing, PyPi readme files are up to date"
+else
+	echo "😿 Outdated PyPi readme files detected."
+	echo "Run the following script to automatically update the readme files:"
+	echo "./scripts/updatePypiReadmeFiles.sh"
+	exit 1
+fi
