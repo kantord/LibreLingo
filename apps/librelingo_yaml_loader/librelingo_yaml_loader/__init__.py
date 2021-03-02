9bc95137ab067cece6a00ed3f9c7a27efc72f8c0
Dániel Kántor <git@daniel-kantor.com>
2021-03-02 16:43:02 +0100

    fix(yaml_loader): fix documentation issues

--- apps/librelingo_yaml_loader/librelingo_yaml_loader/__init__.py
+++ apps/librelingo_yaml_loader/librelingo_yaml_loader/__init__.py
@@ -1,7 +1,7 @@
 """
-Export LibreLingo courses in the JSON format expected by the web app
+Load YAML-based LibreLingo courses in your Python project.
 """
 
 from librelingo_yaml_loader.yaml_loader import load_course
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
