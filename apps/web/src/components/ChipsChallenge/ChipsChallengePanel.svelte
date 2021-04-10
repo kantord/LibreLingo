57895919b96019d6956d2b8d4379a8978ec37b7a
Dániel Kántor <git@daniel-kantor.com>
2021-04-10 21:20:13 +0200

    refactor(web): extract <ChipsChallengePanel />

---
+++ apps/web/src/components/ChipsChallenge/ChipsChallengePanel.svelte
@@ -0,0 +1,46 @@
+<script>
+  import ChallengePanel from "../ChallengePanel"
+
+  export let hasAnswer
+  export let submitted
+  export let skipChallenge
+  export let skipAllChallenges
+  export let finishChallenge
+  export let challenge
+  export let correct
+</script>
+
+{#if !hasAnswer && !submitted}
+  <ChallengePanel
+    message="{null}"
+    buttonText="{null}"
+    skipAction="{skipChallenge}"
+    skipAllAction="{skipAllChallenges}" />
+{/if}
+
+{#if hasAnswer > 0 && !submitted}
+  <ChallengePanel
+    message=""
+    buttonText="Submit"
+    submit
+    skipAction="{skipChallenge}"
+    skipAllAction="{skipAllChallenges}" />
+{/if}
+
+{#if submitted}
+  {#if !correct}
+    <ChallengePanel
+      message="Incorrect solution!"
+      messageDetail="{`Correct answer: ${challenge.formattedSolution}`}"
+      buttonText="Continue"
+      incorrect
+      buttonAction="{finishChallenge}" />
+  {/if}
+  {#if correct}
+    <ChallengePanel
+      message="Correct solution!"
+      buttonText="Continue"
+      correct
+      buttonAction="{finishChallenge}" />
+  {/if}
+{/if}
