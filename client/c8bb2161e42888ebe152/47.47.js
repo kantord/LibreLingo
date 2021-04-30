c6efc508e813d0e2caf54f2d82ecfa79cee58f4f
Dániel Kántor <gitlab@daniel-kantor.com>
2021-04-30 05:47:25 +0000

    Updates

---
+++ client/c8bb2161e42888ebe152/47.47.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[47],{356:function(a){a.exports=JSON.parse('{"id":"434d43b3","levels":1,"challenges":[{"id":6663,"type":"listeningExercise","answer":"agua","audio":"agua","meaning":"asdfg"},{"id":66,"type":"listeningExercise","answer":"agua","audio":"agua","meaning":"asdfg"}]}')}}]);
---
+++ client/c8bb2161e42888ebe152/50.50.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[50],{359:function(e){e.exports=JSON.parse('{"id":"434d43b3","levels":1,"challenges":[{"id":663,"type":"shortInput","phrase":[{"word":"foo"}],"formInTargetLanguage":["leche","la leche"]}]}')}}]);
