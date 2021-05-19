af3628c4de2ced9493de5e9d4a1f7882cbd692c0
Dániel Kántor <git@daniel-kantor.com>
2021-05-19 16:04:06 +0200

    feat(web): add introduction page

---
+++ apps/web/cypress/integration/introductionPage.feature
@@ -0,0 +1,11 @@
+Feature: Skill introduction page
+
+  An introduction page for skills, where the user can read the contents of an (optional)
+  markdown file, which explains the grammar in the skill.
+
+  Scenario: Opening the introduction page
+    And I open "/course/test"
+    And I click "Learn Nature"
+    Then I should be on "/course/test/skill/nature/introduction"
+    Then I read "Introduction to nature"
+
--- apps/web/src/routes/course/[courseName]/skill/[id]/introduction.svelte
+++ apps/web/src/routes/course/[courseName]/skill/[id]/introduction.svelte
@@ -1,17 +1,16 @@
 <script lang="typescript" context="module">
   import MarkDownPage, {
       getMarkDownData,
-  } from "../components/MarkDownPage.svelte"
+} from "../../../../../components/MarkDownPage.svelte"
   import { _ } from "svelte-i18n"
 
   export async function preload() {
       return {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          readmeHTML:
-        // @ts-ignore
-        (
-            await getMarkDownData(await import("../../../../README.md"))
-        ).contents.split("<h2>Tech stack</h2>")[0],
+          readmeHTML: // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      (
+          await getMarkDownData(await import("../../../../README.md"))
+      ).contents.split("<h2>Tech stack</h2>")[0],
       }
   }
 </script>
