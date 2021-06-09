34ddbd4b1407ed8be3aab75fbe4d86f9f962714e
Dániel Kántor <git@daniel-kantor.com>
2021-06-09 18:36:07 +0200

    feat: add repositoryUrl to Course()

--- apps/librelingo_types/librelingo_types/data_types.py
+++ apps/librelingo_types/librelingo_types/data_types.py
@@ -23,6 +23,7 @@ class Course(namedtuple("Course", [
     "modules",
     "license",
     "dictionary",
+    "repositoryUrl",
     "settings",
 ], defaults=[default_settings])):
     """
@@ -41,7 +42,8 @@ class Course(namedtuple("Course", [
             name="CC BY 4.0",
             link="https://creativecommons.org/licenses/by/4.0/"
         ),
-        dictionary=[dict_item1, dict_item2, dict_item3, dict_item4]
+        dictionary=[dict_item1, dict_item2, dict_item3, dict_item4],
+        setttings=Settings()
     )
     ```
     """
