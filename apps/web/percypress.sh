98c6abbc022db9b20a2a91f9765865295a848fee
Dániel Kántor <git@daniel-kantor.com>
2021-07-22 21:11:43 +0200

    chore: temporarily disable percy

--- apps/web/percypress.sh
+++ apps/web/percypress.sh
@@ -4,8 +4,10 @@ if [ "$KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC" = "" ]; then
   KNAPSACK_PRO_ENDPOINT=https://api-disabled-for-fork.knapsackpro.com \
     KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC=disabled-for-fork \
     KNAPSACK_PRO_MAX_REQUEST_RETRIES=0 
-    percy exec -- yarn cy:run
+    # percy exec -- yarn cy:run
+    yarn cy:run
 else
   echo "KNAPSACK PRO ENABLED"
-  KNAPSACK_PRO_TEST_FILE_PATTERN=\"cypress/integration/**/*.{feature,features}\" percy exec -- $(npm bin)/knapsack-pro-cypress
+  # KNAPSACK_PRO_TEST_FILE_PATTERN=\"cypress/integration/**/*.{feature,features}\" percy exec -- $(npm bin)/knapsack-pro-cypress
+  KNAPSACK_PRO_TEST_FILE_PATTERN=\"cypress/integration/**/*.{feature,features}\" $(npm bin)/knapsack-pro-cypress
 fi
