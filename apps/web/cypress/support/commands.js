5490b9b4f5172d2699a60843810ab5f8637240d8
Dániel Kántor <git@daniel-kantor.com>
2021-07-22 21:11:43 +0200

    chore: temporarily disable percy

--- apps/web/cypress/support/commands.js
+++ apps/web/cypress/support/commands.js
@@ -24,6 +24,6 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-import "@percy/cypress"
+// import "@percy/cypress"
 
 require("cy-tkey-screenshot")
