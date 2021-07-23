dd0ee180bd6034eb23a023b568d2434b9c1f08eb
Dániel Kántor <git@daniel-kantor.com>
2021-07-23 20:46:44 +0200

    chore(ci): re-enable percy

--- apps/web/cypress/integration/common/everything_looks_correct.js
+++ apps/web/cypress/integration/common/everything_looks_correct.js
@@ -4,7 +4,7 @@ import { Then } from "cypress-cucumber-preprocessor/steps"
 Then("{} looks correct", (snapshotsName) => {
     cy.get(".fontawesome-i2svg-pending").should("not.exist")
     cy.document().its("fonts.status").should("equal", "loaded")
-    // cy.percySnapshot(snapshotsName)
+    cy.percySnapshot(snapshotsName)
 
     cy.generateTranslationScreenshots()
 })
