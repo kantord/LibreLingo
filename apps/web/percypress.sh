42e59569b9b4dc2deb465d920f8eeb842c36f35a
Daniel Kantor <github@daniel-kantor.com>
2021-07-24 17:19:02 +0200

    chore(ci): re-enable percy (#1470)

--- apps/web/percypress.sh
+++ apps/web/percypress.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if [ "$KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC" = "" ]; then
-  echo "KNAPSACK PRO NOT ENABLED"
-  KNAPSACK_PRO_ENDPOINT=https://api-disabled-for-fork.knapsackpro.com \
-    KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC=disabled-for-fork \
-    KNAPSACK_PRO_MAX_REQUEST_RETRIES=0 
-    # percy exec -- yarn cy:run
-    yarn cy:run
-else
-  echo "KNAPSACK PRO ENABLED"
-  # KNAPSACK_PRO_TEST_FILE_PATTERN=\"cypress/integration/**/*.{feature,features}\" percy exec -- $(npm bin)/knapsack-pro-cypress
-  KNAPSACK_PRO_TEST_FILE_PATTERN=\"cypress/integration/**/*.{feature,features}\" $(npm bin)/knapsack-pro-cypress
-fi
+# if [ "$KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC" = "" ]; then
+#   echo "KNAPSACK PRO NOT ENABLED"
+#   KNAPSACK_PRO_ENDPOINT=https://api-disabled-for-fork.knapsackpro.com \
+#     KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC=disabled-for-fork \
+#     KNAPSACK_PRO_MAX_REQUEST_RETRIES=0 
+#     # percy exec -- yarn cy:run
+#     yarn cy:run
+# else
+#   echo "KNAPSACK PRO ENABLED"
+# KNAPSACK_PRO_TEST_FILE_PATTERN=\"./cypress/integration/**/*.feature\" percy exec -- $(npm bin)/knapsack-pro-cypress
+percy exec -- cypress run
+# fi
