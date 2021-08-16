1e8f8b9dedbdd0bed7b833502af3340f79abe5ec
Dániel Kántor <git@daniel-kantor.com>
2021-08-16 09:10:49 +0200

    feat(web): create custom <Button /> component implementation

---
+++ apps/web/src/components/InputFieldWithVirtualKeyboard/VirtualKeyboard.svelte
@@ -0,0 +1,34 @@
+<script lang="typescript">
+  import { createEventDispatcher } from "svelte"
+  import Button from "../../../../lluis/Button.svelte"
+
+  const dispatch = createEventDispatcher()
+  export let characters: Array<string>
+  export let disabled: boolean
+</script>
+
+<div class="virtual-keyboard">
+  {#each characters as character}
+    <Button
+      tabindex="{-1}"
+      light
+      key
+      size="small"
+      disabled="{disabled}"
+      on:click="{() => dispatch('click', character)}"
+    >
+      {character}
+    </Button>
+  {/each}
+</div>
+
+<style type="text/scss">
+  .virtual-keyboard {
+    display: flex;
+    margin-top: 2em;
+
+    > :global(*) {
+      margin-right: 12px;
+    } 
+  }
+</style>
