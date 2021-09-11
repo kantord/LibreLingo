e5cf3d6119f19230caa094e656564d8a2ef0fe89
Dániel Kántor <git@daniel-kantor.com>
2021-09-11 15:46:45 +0200

    refactor(audios): reuse list_required_audios()

--- apps/librelingo_audios/librelingo_audios/functions.py
+++ apps/librelingo_audios/librelingo_audios/functions.py
@@ -1,4 +1,5 @@
 from librelingo_utils import audio_id, iterate_phrases
+from librelingo_utils.utils import iterate_words
 
 
 def list_required_audios(course):
@@ -7,4 +8,11 @@ def list_required_audios(course):
         # the other versions never need audio.
         text = phrase.in_target_language[0]
         id_ = audio_id(course.target_language, phrase.in_target_language[0])
+        yield [id_, text]
+
+    for word in iterate_words(course):
+        # Returning only the first version because
+        # the other versions never need audio.
+        text = word.in_target_language[0]
+        id_ = audio_id(course.target_language, word.in_target_language[0])
         yield [id_, text]
