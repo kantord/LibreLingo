de950f9620234f06f9d8cfb1807662e448eabfd4
Gabe D <gabe.p.dominguez@gmail.com>
2021-09-18 22:17:04 +0000

    ci: Updates scripts to exit 1 on failures
    This is so if there is an issue, CI will catch it

--- scripts/exportAllYamlCourses.sh
+++ scripts/exportAllYamlCourses.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+ANY_FAILED="0"
 for d in ./courses/*/ ; do
 		d=$(echo $d | sed 's/[^/]*\/[^/]*\///' | sed 's/\///')
 		./scripts/exportYamlCourse.sh $d
+		if [ "$?" -eq "1" ]; then
+			ANY_FAILED="1"
+		fi
 done
+
+if [ "$ANY_FAILED" -eq "1" ]; then
+	echo "😿 Could not export all yaml courses"
+	exit 1
+fi
 
