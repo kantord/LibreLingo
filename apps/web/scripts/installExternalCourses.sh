77033d71a680c152b8dc82afe81932c28aeefa01
Lamdarer <50927784+Lamdarer@users.noreply.github.com>
2021-09-28 22:15:51 +0200

    feat: import german course from external repo
    Update installExternalCourses.sh

--- apps/web/scripts/installExternalCourses.sh
+++ apps/web/scripts/installExternalCourses.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-yarn installCourse kantord/LibreLingo-ES-from-EN spanish-from-english
+yarn installCourse "https://github.com/kantord/LibreLingo-ES-from-EN/archive/refs/heads/main.zip" spanish-from-english
+yarn installCourse https://codeberg.org/Lamdarer/LibreLingo-DE-from-EN/archive/main.zip german-from-english
