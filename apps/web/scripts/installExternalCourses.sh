82f3b0cd57113cdb6107420a904e5db2d5244bbf
Lamdarer <50927784+Lamdarer@users.noreply.github.com>
2021-10-03 15:10:03 +0200

    Update installExternalCourses.sh

--- apps/web/scripts/installExternalCourses.sh
+++ apps/web/scripts/installExternalCourses.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-yarn installCourse kantord/LibreLingo-ES-from-EN spanish-from-english
-yarn installCourse Lamdarer/LibreLingo-DE-from-EN german-from-english
+yarn installCourse "https://github.com/kantord/LibreLingo-ES-from-EN/archive/refs/heads/main.zip" spanish-from-english
+yarn installCourse https://codeberg.org/Lamdarer/LibreLingo-DE-from-EN/archive/main.zip german-from-english
