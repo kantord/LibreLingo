5689b10d1bba04731c0fcf09c2d86f185fecdc15
Peter Cruckshank <petecruckshank@gmail.com>
2021-10-17 12:18:19 -0400

    whoops I used require instead of import 🤷

--- apps/web/src/i18n.js
+++ apps/web/src/i18n.js
@@ -1,6 +1,6 @@
 import { register, init, getLocaleFromNavigator } from "svelte-i18n"
 
-register("en", () => require("./translation/en.json"))
+register("en", () => import("./translation/en.json"))
 register("eo", () => import("./translation/eo.json"))
 register("it", () => import("./translation/it.json"))
 
