ddd48e2ea7aec841063abfd992754598ae5e23ac
Peter Cruckshank <petecruckshank@gmail.com>
2021-10-24 22:01:41 -0400

    removed other dictonaries, left EN and comment ✅

--- apps/web/src/i18n.js
+++ apps/web/src/i18n.js
@@ -1,8 +1,8 @@
 import { register, init, getLocaleFromNavigator } from "svelte-i18n"
 
-register("en", () => import("./translation/en.json"))
-register("eo", () => import("./translation/eo.json"))
-register("it", () => import("./translation/it.json"))
+// Register translations
+register("en", () => require("./translation/en.json"))
+
 
 init({
     fallbackLocale: "en",
