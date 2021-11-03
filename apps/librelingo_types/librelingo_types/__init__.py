b9b26a8f7ff4826b1eb403aac93571b09e51fe3a
Dániel Kántor <git@daniel-kantor.com>
2021-11-03 20:33:13 +0100

    docs: add comments asking not to change pypi pkg versions before merge

--- apps/librelingo_types/librelingo_types/__init__.py
+++ apps/librelingo_types/librelingo_types/__init__.py
@@ -4,4 +4,4 @@ Data types to be used in Python packages for LibreLingo
 
 from librelingo_types.data_types import *
 
-__version__ = "3.2.0"
+__version__ = "3.2.0"  # Do not edit before merge to 'main'
