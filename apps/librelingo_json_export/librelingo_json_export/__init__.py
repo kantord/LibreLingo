7257810c9aaf79698059d2b6c6d5e7ab8e088012
Daniel Kantor <github@daniel-kantor.com>
2021-11-08 19:07:18 +0100

    Get Basque course export speed from 270s to 31s (#1764)
    * chore: ignore german course in .gitignore
    
    * perf: dramatically improve course export performance

--- apps/librelingo_json_export/librelingo_json_export/__init__.py
+++ apps/librelingo_json_export/librelingo_json_export/__init__.py
@@ -4,4 +4,4 @@ Export LibreLingo courses in the JSON format used by the web app
 
 import librelingo_json_export.export
 
-__version__ = "0.9.0"  # Do not edit before merge to 'main'
+__version__ = "0.9.2"  # Do not edit before merge to 'main'
