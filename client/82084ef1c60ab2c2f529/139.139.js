5a7b5c43a8944a08c0a927862152848f4821cb78
Dániel Kántor <github@daniel-kantor.com>
2021-12-12 21:08:55 +0000

    Updates

--- client/82084ef1c60ab2c2f529/139.139.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[139],{442:function(e){e.exports=JSON.parse('{"id":"1611ba1c35a7","levels":1,"challenges":[]}')}}]);
--- client/82084ef1c60ab2c2f529/244.244.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[244],{541:function(n,o,t){"use strict";t.r(o),o.default="# Introduction to animals"}}]);
