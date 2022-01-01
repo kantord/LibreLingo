dabf68cea4eaa04f3f7244cfd5dde4d7fbfa83f8
Piotr Idzik <vil02@o2.pl>
2022-01-01 13:16:47 +0100

    refactor(tests): add test_challenges_get_options_challenge.py

--- apps/librelingo_json_export/tests/test_challenges.py
+++ apps/librelingo_json_export/tests/test_challenges.py
@@ -5,48 +5,9 @@ from unittest import TestCase
 from librelingo_json_export.challenge_types import get_short_input_challenge
 from librelingo_json_export.challenge_types import get_listening_challenge
 from librelingo_json_export.challenge_types import get_chips_challenge
-from librelingo_json_export.challenge_types import get_options_challenge
 from librelingo_json_export.challenge_types import get_chips_from_phrase
 from librelingo_types import Settings, AudioSettings
 from librelingo_fakes import fakes
-
-
-class TestGetOptionsChallenge(TestCase):
-    def test_returns_correct_value1(self):
-        challenge = get_options_challenge(fakes.word1, fakes.course1)[0]
-        expected_challenge = {
-            "id": "db8fd4cec19f",
-            "type": "options",
-            "formInTargetLanguage": "foous",
-            "meaningInSourceLanguage": "foo",
-            "priority": 0,
-            "group": "aab69500f014",
-        }
-        self.assertEqual(challenge, expected_challenge)
-
-    def test_returns_correct_value2(self):
-        challenge = get_options_challenge(fakes.word2, fakes.course1)[0]
-        expected_challenge = {
-            "id": "e50475a646e2",
-            "type": "options",
-            "formInTargetLanguage": "apfel",
-            "meaningInSourceLanguage": "apple",
-            "priority": 0,
-            "group": "9dbe235cb2d6",
-        }
-        self.assertEqual(challenge, expected_challenge)
-
-    def test_returns_correct_value_with_spaces(self):
-        challenge = get_options_challenge(fakes.word_with_spaces, fakes.course1)[0]
-        expected_challenge = {
-            "id": "98220c0c74ac",
-            "type": "options",
-            "formInTargetLanguage": "three word term",
-            "meaningInSourceLanguage": "foo bar baz",
-            "priority": 0,
-            "group": "e707f76a703d",
-        }
-        self.assertEqual(challenge, expected_challenge)
 
 
 class TestGetShortInputChallenge(TestCase):
