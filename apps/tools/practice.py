b778fd0420a35dd55b2720f4d04f4f2c7acf3c6c
Dániel Kántor <git@daniel-kantor.com>
2022-01-01 12:57:43 +0100

    feat: deploy librelingo tools output to github pages

--- apps/tools/practice.py
+++ apps/tools/practice.py
@@ -2,7 +2,7 @@
 import argparse
 import random
 
-from lili import guess_path_to_course, collect_data, collect_words, collect_phrases
+from librelingo_tools import guess_path_to_course, collect_data, collect_words, collect_phrases
 from librelingo_yaml_loader.yaml_loader import load_course
 
 
