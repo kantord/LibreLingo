fdb91254f1925e4306dafc35a38ca9ea92cd5507
Qbecb1zen <aminelouzar@hotmail.cm>
2022-01-02 20:05:12 +0100

    refactor clean word tests

---
+++ apps/librelingo_json_export/tests/test_challenges_clean_word.py
@@ -0,0 +1,24 @@
+from utils import FakeLanguages
+from pyfakefs.fake_filesystem_unittest import TestCase
+from librelingo_utils import clean_word
+
+def test_empty_string():
+    assert clean_word("")==""
+
+def test_removes_parentheses():
+    assert clean_word("(foo")==FakeLanguages.LANG_1
+
+def test_removes_comma():
+    assert clean_word("foo,")==FakeLanguages.LANG_1
+
+def test_doesnt_remove_parts_of_word_1():
+    assert clean_word("ba-ar")=="ba-ar"
+
+def test_doesnt_remove_parts_of_word_2():
+    assert clean_word("L'Hospitalet")=="L'Hospitalet"
+
+def test_removes_exclamation_mark():
+    assert clean_word("ba-ar!")=="ba-ar"
+
+def test_weird_english_posessive():
+    assert clean_word("cats'")=="cats'"
