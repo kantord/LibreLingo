db5dbda137ef64eaf44244820b8ba37514ba6f47
Qbecb1zen <aminelouzar@hotmail.cm>
2022-01-02 22:57:06 +0100

    improv: fix ci errors

--- apps/librelingo_json_export/tests/utils.py
+++ apps/librelingo_json_export/tests/utils.py
@@ -1,11 +1,6 @@
 from enum import Enum
-import random
 
 
 class FakeLanguages(str, Enum):
     LANG_1 = "foo"
     LANG_2 = "bar"
-
-
-def generate_random_int(number: int):
-    return random.randint(0, number)
