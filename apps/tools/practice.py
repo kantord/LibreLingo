fe0cfd09e019c34b7708754dd20c537aa2601a0f
Gabor Szabo <gabor@szabgab.com>
2022-01-04 16:45:59 +0200

    feat(devtools) extract the guess_word function

--- apps/tools/practice.py
+++ apps/tools/practice.py
@@ -27,35 +27,41 @@ def _show_banner():
     print()
 
 
+def guess_word(mapper):
+    words = list(mapper.keys())
+    word = random.choice(words)
+    translation = input(f"{word}: ")
+    if translation == "x":
+        return True
+    match = check(translation, mapper, word)
+    print("+" if match else "-")
+    for trans in mapper[word]:
+        print(f"   {trans}")
+    if not match:
+        translation = input("Try again: ")
+        if translation == "x":
+            return True
+        match = check(translation, mapper, word)
+        print("++" if match else "--")
+
+    print()
+    return False
+
+
 def main():
     args = get_args()
     path_to_course = guess_path_to_course(args.course)
     course = load_course(path_to_course)
-    target, source, count = collect_data(course)
+    _, source, _ = collect_data(course)
     _show_banner()
 
     # Randomly select a word, show it, ask the user to type the answer, verify the answer
     # Same with phrases
 
     source_to_target_words = collect_words(source, "source-to-target")
-    source_words = list(source_to_target_words.keys())
     while True:
-        word = random.choice(source_words)
-        translation = input(f"{word}: ")
-        if translation == "x":
+        if guess_word(source_to_target_words):
             break
-        match = check(translation, source_to_target_words, word)
-        print("+" if match else "-")
-        for trans in source_to_target_words[word]:
-            print(f"   {trans}")
-        if not match:
-            translation = input("Try again: ")
-            if translation == "x":
-                break
-            match = check(translation, source_to_target_words, word)
-            print("++" if match else "--")
-
-        print()
 
 
 if __name__ == "__main__":
