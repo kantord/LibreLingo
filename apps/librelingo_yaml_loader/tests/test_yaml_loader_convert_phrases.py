a5243805a9e740a158a881beac03e4670d3ac45a
Piotr Idzik <vil02@o2.pl>
2022-01-04 17:38:59 +0100

    fix(tests): add mock_convert_phrase to test_converts_every_word

--- apps/librelingo_yaml_loader/tests/test_yaml_loader_convert_phrases.py
+++ apps/librelingo_yaml_loader/tests/test_yaml_loader_convert_phrases.py
@@ -14,14 +14,17 @@ def test_returns_a_list():
     assert isinstance(_convert_phrases([]), list)
 
 
-def test_converts_every_word():
-    raw_words = [None] * tu.get_some_int()
-    assert len(_convert_phrases(raw_words)) == len(raw_words)
-
-
 @pytest.fixture
 def mock_convert_phrase(mocker):
     return mocker.patch("librelingo_yaml_loader.yaml_loader._convert_phrase")
+
+
+def test_converts_every_word(mock_convert_phrase):
+    # pylint: disable=unused-argument
+    # mock_convert_phrase is needed here becasue
+    # _convert_phrase crashes for input None
+    raw_words = [None] * tu.get_some_int()
+    assert len(_convert_phrases(raw_words)) == len(raw_words)
 
 
 def test_returns_correct_value(mock_convert_phrase):
