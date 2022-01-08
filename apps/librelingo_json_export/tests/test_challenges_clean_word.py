992c6fd96437cca9b4b57d2239fda5bc9f50ab25
qbecb1zen <aminelouzar@hotmail.com>
2022-01-08 01:10:53 +0100

    improv: change variables to real life ones

--- apps/librelingo_json_export/tests/test_challenges_clean_word.py
+++ apps/librelingo_json_export/tests/test_challenges_clean_word.py
@@ -6,23 +6,23 @@ def test_empty_string():
 
 
 def test_removes_parentheses():
-    assert clean_word("(foo") == "foo"
+    assert clean_word("(Hello there") == "Hello there"
 
 
 def test_removes_comma():
-    assert clean_word("foo,") == "foo"
+    assert clean_word("This is,") == "This is"
 
 
 def test_doesnt_remove_parts_of_word_1():
-    assert clean_word("ba-ar") == "ba-ar"
+    assert clean_word("Mother-in-law") == "Mother-in-law"
 
 
 def test_doesnt_remove_parts_of_word_2():
-    assert clean_word("L'Hospitalet") == "L'Hospitalet"
+    assert clean_word("It's him") == "It's him"
 
 
 def test_removes_exclamation_mark():
-    assert clean_word("ba-ar!") == "ba-ar"
+    assert clean_word("It's his mother-in-law!") == "It's his mother-in-law"
 
 
 def test_weird_english_posessive():
