8b61aa4e097c485c4b6b0f6b56f67a975c660e79
vil02 <vil02@o2.pl>
2022-01-14 18:35:47 +0100

    fix: revert changes

--- scripts/updateAudioForYamlCourse.sh
+++ scripts/updateAudioForYamlCourse.sh
@@ -2,7 +2,8 @@
 
 echo -en "⏳ Updating audio for course $1"
 cd ./apps/librelingo_audios/ > /dev/null
-if poetry run python librelingo_audios/cli.py ../../courses/"$1" ../../apps/web/static/voice "$1" "${@:2}"; then
+poetry run python librelingo_audios/cli.py ../../courses/"$1" ../../apps/web/static/voice "$1" "${@:2}"
+if [ $? -eq 0 ]; then
 		echo -en "\r✅ Updated audio for course $1"
 else
 		echo -en "\r⚠️  Couldn't update audio for course $1"
