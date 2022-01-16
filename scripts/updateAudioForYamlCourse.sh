2a5a325779f0d38455fa4324e9e45d234360193f
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2022-01-16 15:09:40 +0100

    fix: avoid potential bugs (#1999)
    * fix: use double quotes
    
    * fix: use $(...)
    
    * fix: remove redundant 'cd -'
    
    * style: avoid using $?
    
    * style: avoid using $?
    
    * fix: revert changes
    
    * fix: use safe cd

--- scripts/updateAudioForYamlCourse.sh
+++ scripts/updateAudioForYamlCourse.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 echo -en "⏳ Updating audio for course $1"
-cd ./apps/librelingo_audios/ > /dev/null
-poetry run python librelingo_audios/cli.py ../../courses/$1 ../../apps/web/static/voice $1 "${@:2}"
+cd ./apps/librelingo_audios/ ||
+{
+  echo -en "\r⚠️  Wrong folder structure"
+  exit 1
+}
+poetry run python librelingo_audios/cli.py ../../courses/"$1" ../../apps/web/static/voice "$1" "${@:2}"
 if [ $? -eq 0 ]; then
 		echo -en "\r✅ Updated audio for course $1"
 else
 		echo -en "\r⚠️  Couldn't update audio for course $1"
 fi
-cd - > /dev/null
 echo
