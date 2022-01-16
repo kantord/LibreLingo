67b7ab11365a59513de4e6d8c636ef1dc382130d
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2022-01-16 15:09:40 +0100

    fix: avoid potential bugs (#1999)
    * fix: use double quotes
    
    * fix: use $(...)
    
    * fix: remove redundant 'cd -'
    
    * style: avoid using $?
    
    * style: avoid using $?
    
    * fix: revert changes
    
    * fix: use safe cd

--- scripts/exportYamlCourse.sh
+++ scripts/exportYamlCourse.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 echo -en "⏳ Exporting course $1"
-cd ./apps/librelingo_json_export/ > /dev/null
-poetry run export-cli ../../courses/$1 ../../apps/web/src/courses/$1
+cd ./apps/librelingo_json_export/ ||
+{
+  echo -en "\r⚠️  Wrong folder structure"
+  exit 1
+}
+poetry run export-cli ../../courses/"$1" ../../apps/web/src/courses/"$1"
 if [ $? -eq 0 ]; then
 		echo -en "\r✅ Exported course $1"
 else
 		echo -en "\r⚠️  Couldn't export course $1"
 		exit 1
 fi
-cd - > /dev/null
 echo
