55d965e90fb7ffa2da7c78728117bfe6092cc3e9
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2022-01-16 13:46:25 +0100

    refactor(tests): add test_export_course_skills.py (#1992)
    * refactor(tests): add test_export_course_skills.py
    
    * fix(tests): add get_fake_skill
    
    * fix(tests): remove unused imports
    
    * refactor(tests): move get_fake_skill() to fakes
    
    * refactor(tests): add test_export_skill.py
    
    * fix: remove fakes.
    
    * fix: remove fakes.
    
    * fix(tests): remove unused import
    
    * fix(tests): use proper import order
    
    * fix(tests): use caplog
    
    * fix(tests): use proper order of entries in caplog.record_tuples
    
    * refactor(tests): add test_export_course_data.py
    
    * refactor(tests): add test_export_course.py
    
    Co-authored-by: Daniel Kantor <github@daniel-kantor.com>

---
+++ apps/librelingo_json_export/tests/test_export_course.py
@@ -0,0 +1,33 @@
+import pytest
+
+from librelingo_json_export.export import export_course
+from librelingo_fakes import fakes
+
+
+@pytest.fixture
+def export_path():
+    return fakes.path()
+
+
+@pytest.fixture
+def mock_export_course_data(mocker):
+    return mocker.patch("librelingo_json_export.export._export_course_data")
+
+
+def test_calls__export_course_data_with_correct_value(
+    fs, export_path, mock_export_course_data
+):  # pylint:disable=invalid-name
+    export_course(export_path, fakes.course1)
+    mock_export_course_data.assert_called_with(export_path, fakes.course1, None)
+
+
+@pytest.fixture
+def mock_export_course_skills(mocker):
+    return mocker.patch("librelingo_json_export.export._export_course_skills")
+
+
+def test_calls__export_course_skills_with_correct_value(
+    fs, export_path, mock_export_course_skills
+):  # pylint:disable=invalid-name
+    export_course(export_path, fakes.course1)
+    mock_export_course_skills.assert_called_with(export_path, fakes.course1, None)
---
+++ apps/librelingo_json_export/tests/test_export_skill.py
@@ -0,0 +1,75 @@
+import os
+import json
+import logging
+import pytest
+
+from librelingo_json_export.export import _export_skill
+
+from librelingo_fakes import fakes
+
+
+@pytest.fixture
+def export_path():
+    return fakes.path()
+
+
+def test_creates_the_challenges_file(fs, export_path):  # pylint:disable=invalid-name
+    random_name, fake_skill = fakes.get_fake_skill()
+    _export_skill(export_path, fake_skill, fakes.course1)
+    assert os.path.exists(export_path / "challenges" / f"animals-{random_name}.json")
+
+
+def test_creates_the_introduction_file(fs, export_path):  # pylint:disable=invalid-name
+    fake_name = str(fakes.fake_value())
+    introduction = f"# *Hello* (https://example.com)[_{fake_name}_]!"
+    random_name, fake_skill = fakes.get_fake_skill(introduction=introduction)
+    _export_skill(export_path, fake_skill, fakes.course1)
+    with open(
+        export_path / "introduction" / f"animals-{random_name}.md"
+    ) as animals_file:
+        introduction_file_content = animals_file.read()
+        assert introduction_file_content == introduction
+
+
+def test_does_not_create_an_introduction_file_if_theres_no_introduction(
+    fs, export_path
+):  # pylint:disable=invalid-name
+    random_name, fake_skill = fakes.get_fake_skill()
+    _export_skill(export_path, fake_skill, fakes.course1)
+    assert not os.path.exists(
+        export_path / "introduction" / f"animals-{random_name}.md"
+    )
+
+
+@pytest.fixture
+def mock_get_skill_data(mocker):
+    return mocker.patch("librelingo_json_export.export._get_skill_data")
+
+
+def test_calls__get_skill_data_with_correct_value(
+    fs, export_path, mock_get_skill_data
+):  # pylint:disable=invalid-name
+    mock_get_skill_data.return_value = []
+    _export_skill(export_path, fakes.skillWithPhraseAndWord, fakes.course1)
+    mock_get_skill_data.assert_called_with(fakes.skillWithPhraseAndWord, fakes.course1)
+
+
+def test_writes_correct_value_into_json_file(
+    fs, export_path, mock_get_skill_data
+):  # pylint:disable=invalid-name
+    fake_skill_data = {"fake_skill_data": 1000}
+    mock_get_skill_data.return_value = fake_skill_data
+    _export_skill(export_path, fakes.skillWithPhraseAndWord, fakes.course1)
+    with open(export_path / "challenges" / "masculine.json") as masculine_file:
+        assert json.loads(masculine_file.read()) == fake_skill_data
+
+
+def test_assert_logs_correctly(fs, caplog, export_path):  # pylint:disable=invalid-name
+    with caplog.at_level(logging.INFO, logger="librelingo_json_export"):
+        _, fake_skill = fakes.get_fake_skill()
+        _export_skill(export_path, fake_skill, fakes.course1)
+        assert caplog.record_tuples[0] == (
+            "librelingo_json_export",
+            logging.INFO,
+            f"Writing skill '{fake_skill.name}'",
+        )
