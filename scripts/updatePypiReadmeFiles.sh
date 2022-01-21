4858d492822d5f2496201f4cf0b0f4861242de1e
vil02 <vil02@o2.pl>
2022-01-21 21:24:01 +0100

    fix: add warning

--- scripts/updatePypiReadmeFiles.sh
+++ scripts/updatePypiReadmeFiles.sh
@@ -12,7 +12,10 @@ find apps/ -name pyproject.toml -exec dirname {} \; |
 				exit 1
 			}
 			poetry install
-			make README.md -B
+			make README.md -B ||
+      {
+        echo -en "\r⚠️Could not update README.md for Python package '$(basename "$directory")'..."
+      }
 			cd - ||
 			{
 				echo -en "\r⚠️  Could not return to previous directory"
