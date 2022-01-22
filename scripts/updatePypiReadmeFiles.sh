226c1865e8cf294b3c1863349afac079360b2afa
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2022-01-22 15:39:01 +0100

    feat: use shellcheck in CI (#2010)
    * fix: use while read with find
    
    * style: use safe cd
    
    * feat: add checkSellScripts.sh
    
    * fix: use set -euo pipefail
    
    * fix: use "set -euo pipefail"
    
    * fix: use "set -euo pipefail"
    
    * fix: use "set -euo pipefail"
    
    * fix: use "set -euo pipefail"
    
    * fix: use "set -euo pipefail"
    
    * fix: use "set -euo pipefail"
    
    * fix: use "set -euo pipefail"
    
    * chore: update repository URL in configuration file
    
    * fix: update links to point to https://github.com/LibreLingo/LibreLingo instead of https://github.com/kantord/LibreLingo
    
    * fix: linking the contributors batch to the contributors section
    
    * fix: use "set -euo pipefail"
    
    * fix: revert change
    
    * fix: use bash
    
    * fix: use "set -euo pipefail"
    
    * style: avoid using $?
    
    * style: avoid using $?
    
    * fix: remove redundant command line arguments
    
    * fix: use "set -euo pipefail"
    
    * fix: add warning
    
    * fix: fix spacing
    
    * feat: add check_shell_scripts.sh
    
    Co-authored-by: Gabor Szabo <gabor@szabgab.com>

--- scripts/updatePypiReadmeFiles.sh
+++ scripts/updatePypiReadmeFiles.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-for directory in $(find apps/ -name pyproject.toml | xargs -n1 dirname); do
-	echo "Updating README.md for Python package '$(basename "$directory")'..."
-	cd "$directory" ||
-	{
-		echo -en "\r⚠️  Could not enter $directory"
-		exit 1
-	}
-	poetry install
-	make README.md -B "$1"
-	cd -
-	echo
-done
+set -euo pipefail
+
+find apps/ -name pyproject.toml -exec dirname {} \; |
+  while IFS="" read -r directory
+    do
+			echo "Updating README.md for Python package '$(basename "$directory")'..."
+			cd "$directory" ||
+			{
+				echo -en "\r⚠️  Could not enter $directory"
+				exit 1
+			}
+			poetry install
+			make README.md -B ||
+      {
+        echo -en "\r⚠️  Could not update README.md for Python package '$(basename "$directory")'..."
+      }
+			cd - ||
+			{
+				echo -en "\r⚠️  Could not return to previous directory"
+				exit 1
+			}
+			echo
+    done
