a540f771587c1a42fab97f4d682cfa354fbb196e
Gabor Szabo <gabor@szabgab.com>
2022-01-23 17:39:47 +0200

    feat(web): allow user to Cancel session at any moment

--- apps/web/cypress/integration/common/i_see_the_challenge_panel.js
+++ apps/web/cypress/integration/common/i_see_the_challenge_panel.js
@@ -1,6 +1,6 @@
 import { Then } from "cypress-cucumber-preprocessor/steps"
 
-Then("I see the challenge panel with no skip button", () => {
+Then("I see the challenge panel with no Skip button", () => {
     cy.get(".panel").should("be.visible")
     cy.get(".panel button").contains("Skip").should("not.exist")
 })
@@ -9,29 +9,29 @@ Then("I see the challenge panel", () => {
     cy.get(".panel").should("be.visible")
 })
 
-Then("I see a panel with only a skip button", () => {
+Then("I see a panel with only a Skip button", () => {
     cy.wait(500) // This is necessary due to the animation
     cy.get(".panel button").contains("Skip").should("be.visible")
     cy.get(".panel ").find("button").should("have.length", 1)
 })
 
-Then("I see a panel with only a skip button and a can't listen now button", () => {
+Then("I see a panel with only a Skip, a Cancel, and a Can't listen now button", () => {
     cy.wait(500) // This is necessary due to the animation
     cy.get(".panel button").contains("Skip").should("be.visible")
     cy.get(".panel button").contains("Can't listen now").should("be.visible")
+    cy.get(".panel ").find("button").should("have.length", 3)
+})
+
+Then("I see a panel with only a Skip and a Cancel button", () => {
+    cy.wait(500) // This is necessary due to the animation
+    cy.get(".panel button").contains("Skip").should("be.visible")
+    cy.get(".panel button").contains("Cancel").should("be.visible")
     cy.get(".panel ").find("button").should("have.length", 2)
 })
 
-Then("I see a panel with only a skip and a finish early button", () => {
+Then("I see a panel with only a Skip and a Cancel button", () => {
     cy.wait(500) // This is necessary due to the animation
     cy.get(".panel button").contains("Skip").should("be.visible")
-    cy.get(".panel button").contains("Finish early").should("be.visible")
-    cy.get(".panel ").find("button").should("have.length", 2)
-})
-
-Then("I see a panel with only a skip and a finish early button", () => {
-    cy.wait(500) // This is necessary due to the animation
-    cy.get(".panel button").contains("Skip").should("be.visible")
-    cy.get(".panel button").contains("Finish early").should("be.visible")
+    cy.get(".panel button").contains("Cancel").should("be.visible")
     cy.get(".panel ").find("button").should("have.length", 3)
 })
