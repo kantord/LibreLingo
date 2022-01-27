60bba7129f861829ccd3ed9465a197a1da20a64f
Gabor Szabo <gabor@szabgab.com>
2022-01-27 08:08:18 +0200

    fix: central courses configuration file

--- scripts/exportYamlCourse.sh
+++ scripts/exportYamlCourse.sh
@@ -8,7 +8,7 @@ cd ./apps/librelingo_json_export/ ||
   echo -en "\r⚠️  Wrong folder structure"
   exit 1
 }
-if poetry run export-cli ../../courses/"$1" ../../apps/web/src/courses/"$1"; then
+if poetry run librelingo-export-cli ../../courses/"$1" ../../apps/web/src/courses/"$1"; then
 		echo -en "\r✅ Exported course $1"
 else
 		echo -en "\r⚠️  Couldn't export course $1"
