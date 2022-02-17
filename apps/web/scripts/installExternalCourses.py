821ac88ddaad16b006586c3aa78170af87e6a4e1
Dániel Kántor <git@daniel-kantor.com>
2022-02-17 07:29:19 +0100

    feat: replace exit() with sys.exit()

--- apps/web/scripts/installExternalCourses.py
+++ apps/web/scripts/installExternalCourses.py
@@ -5,7 +5,7 @@ import os
 
 def main():
     if len(sys.argv) != 2:
-        exit(f"Usage: {sys.argv[0]} PATH_TO_CONFIG_FILE")
+        sys.exit(f"Usage: {sys.argv[0]} PATH_TO_CONFIG_FILE")
     config_filename = sys.argv[1]
     with open(config_filename) as fh:
         config = json.load(fh)
@@ -16,7 +16,7 @@ def main():
             print(cmd)
             exit_code = os.system(cmd)
             if exit_code != 0:
-                exit("We failed to install {course['tdir']}. Terminating")
+                sys.exit("We failed to install {course['tdir']}. Terminating")
 
 
 if __name__ == "__main__":
