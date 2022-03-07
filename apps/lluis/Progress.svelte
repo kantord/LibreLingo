dccc1af0ac68124fe9da4f96091cc586e264c0ff
Dániel Kántor <git@daniel-kantor.com>
2022-03-07 07:35:20 +0100

    feat(lluis): add <Card />

---
+++ apps/lluis/Progress.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let value = 0.0
+</script>
+
+<progress value={value} max="1">
+  {value * 100}%
+</progress>
+
+<style>
+  progress {
+    appearance: none;
+    display: block;
+    width: 100%;
+    height: 1rem;
+    border-radius: 6px;
+    overflow: hidden;
+    border: none;
+    background: blue;
+    padding: 0;
+  }
+
+  progress::-webkit-progress-bar {
+    background-color: red;
+  }
+
+  progress::-webkit-progress-value {
+    background-color: green;
+  }
+
+  progress::-moz-progress-bar {
+    background-color: red;
+  }
+
+  progress::-moz-progress-value {
+    background-color: green;
+  }
+</style>
