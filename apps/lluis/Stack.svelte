411394bb392a6eceffdbced714415029c8cef7d4
Dániel Kántor <git@daniel-kantor.com>
2022-03-07 07:35:20 +0100

    feat(lluis): add <Card />

---
+++ apps/lluis/Stack.svelte
@@ -0,0 +1,17 @@
+<script lang="typescript">
+  export let direction: "row" | "column" = "row"
+
+  let compiledStyle = `
+  flex-direction: ${direction};
+  `
+</script>
+
+<div style={compiledStyle}>
+  <slot />
+</div>
+
+<style>
+  div {
+    display: flex;
+  }
+</style>
