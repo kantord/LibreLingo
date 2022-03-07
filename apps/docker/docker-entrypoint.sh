1badf5cc3039c7f09b170e5ef6a8872badacd84b
CutThroat <55604778+cutthroat78@users.noreply.github.com>
2022-03-07 11:34:26 +0000

    Added git pull and changed env name

--- apps/docker/docker-entrypoint.sh
+++ apps/docker/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euo pipefail
-if [ "GIT_CLONE" = true ] ; then
+if [ "GIT_REPO" = true ] ; then
     cd /
     git clone https://github.com/LibreLingo/LibreLingo.git
+    cd /LibreLingo
+    git pull
 fi
 yarn exportAllCourses
 yarn web dev
