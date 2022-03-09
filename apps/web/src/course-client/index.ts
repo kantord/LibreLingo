c36a4fc34f961c1a9af28b3f48bd698bfd8850bd
Dániel Kántor <git@daniel-kantor.com>
2022-03-09 22:15:58 +0100

    refactor: extract get_course()

---
+++ apps/web/src/course-client/index.ts
@@ -0,0 +1,76 @@
+import { getMarkDownData } from "../../src/components/MarkDownPage.svelte"
+
+export type SkillDataType = {
+  id: string;
+  practiceHref: string;
+  title: string;
+  levels: number;
+  summary: string[];
+}
+
+export type ModuleDataType = {
+  title: string;
+  skills: SkillDataType[];
+}
+
+export type CourseDataType = {
+  courseName: string;
+  modules: ModuleDataType[];
+  languageName: string;
+  repositoryURL: string;
+  languageCode: string;
+  specialCharacters: string[];
+}
+
+export const get_course = async ({ courseName }: { courseName: string }): CourseDataType => {
+    const { modules, languageName, repositoryURL, languageCode, specialCharacters } = require(
+        `../courses/${courseName}/courseData.json`
+    )
+
+    return { courseName, modules, languageName, repositoryURL, languageCode, specialCharacters }
+}
+
+export const get_skill_data = async ({ courseName, skillName }: { courseName: string, skillName: string }) => {
+    const {
+        languageName,
+        languageCode,
+        specialCharacters,
+        repositoryURL,
+    } = await get_course({ courseName })
+    const skillData = require(
+        `../courses/${courseName}/challenges/${skillName}.json`
+    )
+    const rawChallenges = skillData.challenges
+    const challengesPerLevel = skillData.challenges.length / skillData.levels
+
+    const skillId = skillData.id
+
+    return {
+        rawChallenges: Array.from(rawChallenges),
+        languageName,
+        languageCode,
+        specialCharacters,
+        repositoryURL,
+        skillName,
+        skillId,
+        challengesPerLevel,
+        courseURL: `/course/${courseName}`,
+    }
+}
+
+export const get_skill_introduction = async ({ courseName, skillName }: { courseName: string, skillName: string })=> {
+    const { modules } = await get_course({ courseName })
+    console.log(modules)
+    for (const module of modules) {
+        for (const skill of module.skills) {
+            if (skill.practiceHref === skillName) {
+                return {
+                    courseName,
+                    title: skill.title,
+                    practicehref: skill.practiceHref,
+                    readmeHTML: await getMarkDownData(require(`../courses/${courseName}/introduction/${skill.introduction}`))
+                }
+            }
+        }
+    }
+}
