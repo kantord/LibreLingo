80b5ab4ff1f7855f901c3af7a376b0561800dc78
Dániel Kántor <git@daniel-kantor.com>
2022-03-09 22:15:58 +0100

    refactor: extract get_course()

---
+++ apps/web/src/course-client/__tests__/index.test.ts
@@ -0,0 +1,19 @@
+import { get_course, get_skill_data, get_skill_introduction } from "../index"
+
+describe("get_course", () => {
+    it("returns correct course data", async () => {
+        expect(await get_course({ courseName: "test" })).toMatchSnapshot()
+    })
+})
+
+describe("get_skill_data", () => {
+    it("returns correct course data", async () => {
+        expect(await get_skill_data({ courseName: "test", skillName: "animals" })).toMatchSnapshot()
+    })
+})
+
+describe("get_skill_introduction", () => {
+    it("returns correct course data", async () => {
+        expect(await get_skill_introduction({ courseName: "test", skillName: "animals" })).toMatchSnapshot()
+    })
+})
---
+++ apps/web/src/routes/course/[courseName]/skill/[skillName]/index.svelte
@@ -0,0 +1,46 @@
+<script lang="typescript" context="module">
+  import { get_skill_data } from "../../../../../course-client"
+
+  export async function preload(page) {
+      const { skillName, courseName } = page.params
+      return get_skill_data({ skillName, courseName })
+  }
+</script>
+
+<script lang="typescript">
+  import ChallengeScreen from "../../../../../components/ChallengeScreen.svelte"
+  import NavBar from "../../../../../components/NavBar.svelte"
+  import { sortChallengeGroups } from "./_logic"
+
+  export let rawChallenges
+  export let languageName: string
+  export let languageCode: string
+  export let specialCharacters: Array<string>
+  export let repositoryURL: string
+  export let skillName: string
+  export let courseURL: string
+  export let skillId: string
+  export let challengesPerLevel: number
+
+  let expectedNumberOfChallenges = Math.max(
+      4,
+      Math.round(challengesPerLevel * 1.2)
+  )
+</script>
+
+<svelte:head>
+  <title>LibreLingo - learn {skillName} in {languageName} for free</title>
+</svelte:head>
+
+<NavBar repositoryURL="{repositoryURL}" />
+
+<ChallengeScreen
+  expectedNumberOfChallenges="{expectedNumberOfChallenges}"
+  skillId="{skillId}"
+  rawChallenges="{rawChallenges}"
+  languageName="{languageName}"
+  languageCode="{languageCode}"
+  specialCharacters="{specialCharacters}"
+  sortChallengeGroups="{sortChallengeGroups}"
+  courseURL="{courseURL}"
+/>
