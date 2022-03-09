6cd28e7c574e44c46d9e1d4e058b5d0bac532677
Dániel Kántor <git@daniel-kantor.com>
2022-03-09 22:15:58 +0100

    refactor: extract get_course()

--- jest.config.js
+++ jest.config.js
@@ -173,6 +173,9 @@ module.exports = {
 
     // A map from regular expressions to paths to transformers
     // transform: undefined,
+    "transform": {
+      "^.+\\.md?$": "markdown-loader-jest"
+    },
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
     // transformIgnorePatterns: [
