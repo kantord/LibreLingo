dfe99712b354615a92680415d2d59fdb0a6b03e4
Gabor Szabo <gabor@szabgab.com>
2022-03-15 18:10:24 +0100

    feat: allow loading course data from github gists

--- apps/web/src/course-client/index.ts
+++ apps/web/src/course-client/index.ts
@@ -1,4 +1,6 @@
+import fetch from "cross-fetch"
 import loadMarkdownModule from "../utils/loadMarkdownModule"
+import { baseURL } from "../../../../config/gists.json"
 
 export type SkillDataType = {
   id: string
@@ -23,18 +25,14 @@ export type CourseDataType = {
   specialCharacters: string[]
 }
 
-export const get_course = async ({
-  courseName,
-}: {
-  courseName: string
-}): Promise<CourseDataType> => {
+const formatCourseData = (rawCourseData, { courseName }) => {
   const {
     modules,
     languageName,
     repositoryURL,
     languageCode,
     specialCharacters,
-  } = require(`../courses/${courseName}/courseData.json`) // eslint-disable-line @typescript-eslint/no-var-requires
+  } = rawCourseData
 
   return {
     courseName,
@@ -46,15 +44,56 @@ export const get_course = async ({
   }
 }
 
+const fetchGistFiles = async (gistId) => {
+  // get the data from a Github gist served through a CORS proxy
+  try {
+    const rawResponse = await fetch(`${baseURL}/${gistId}`)
+    const response = await rawResponse.json()
+
+    return Object.fromEntries(
+      Object.entries(response.files).map(([filename, value]) => [
+        filename.replace("librelingo___", "").replace(".json", ""),
+        filename.endsWith(".json") ? JSON.parse(value.content) : value.content,
+      ])
+    )
+  } catch (error) {
+    throw new Error(`Could not load gist with Id "${gistId}". ${error}`)
+  }
+}
+
+export const get_course = async ({
+  courseName,
+  gistId = null,
+}: {
+  courseName: string
+  gistId?: string
+}): Promise<CourseDataType> => {
+  if (gistId !== null) {
+    const { courseData } = await fetchGistFiles(gistId)
+    return formatCourseData(courseData, { courseName })
+  }
+
+  const rawCourseData = require(`../courses/${courseName}/courseData.json`) // eslint-disable-line @typescript-eslint/no-var-requires
+
+  return formatCourseData(rawCourseData, { courseName })
+}
+
 export const get_skill_data = async ({
   courseName,
   skillName,
+  gistId = null,
 }: {
   courseName: string
   skillName: string
+  gistId?: string
 }) => {
   const { languageName, languageCode, specialCharacters, repositoryURL } =
     await get_course({ courseName })
+
+  if (gistId !== null) {
+    throw new Error(`Could not load gist with Id "${gistId}".`)
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const skillData = require(`../courses/${courseName}/challenges/${skillName}.json`)
   const rawChallenges = skillData.challenges
