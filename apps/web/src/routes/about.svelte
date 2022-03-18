c88fb4dc3e97dedacb72d97208a95cc7bd609168
Dániel Kántor <git@daniel-kantor.com>
2022-03-18 07:50:13 +0100

    feat: allow loading course data from github gists

--- apps/web/src/routes/about.svelte
+++ apps/web/src/routes/about.svelte
@@ -1,17 +1,16 @@
 <script lang="typescript" context="module">
-  import MarkDownPage, {
-      getMarkDownData,
-  } from "../components/MarkDownPage.svelte"
+  import MarkDownPage from "../components/MarkDownPage.svelte"
+  import parseMarkdown from "../utils/parseMarkdown"
   import { _ } from "svelte-i18n"
 
   export async function preload() {
-      return {
-          readmeHTML: // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      (await getMarkDownData(await import("../../../../README.md"))).split(
-          "<h2>Tech stack</h2>"
-      )[0],
-      }
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const { default: rawMarkdown } = await import("../../../../README.md")
+
+    return {
+      readmeHTML: parseMarkdown(rawMarkdown).split("<h2>Tech stack</h2>")[0],
+    }
   }
 </script>
 
@@ -20,7 +19,7 @@
 </script>
 
 <MarkDownPage
-  readmeHTML="{readmeHTML}"
+  {readmeHTML}
   title="About LibreLingo"
-  description="{$_('about.meta.description')}"
+  description={$_("about.meta.description")}
 />
