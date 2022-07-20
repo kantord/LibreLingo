7d0ed130051c9ec8d236be09397504aaf2e40908
Dániel Kántor <git@daniel-kantor.com>
2022-07-20 08:56:36 +0200

    perf(yaml-loader): improve performance of loading YAML

--- apps/librelingo_yaml_loader/librelingo_yaml_loader/__init__.py
+++ apps/librelingo_yaml_loader/librelingo_yaml_loader/__init__.py
@@ -4,4 +4,4 @@ Load YAML-based LibreLingo courses in your Python project.
 
 from librelingo_yaml_loader.yaml_loader import load_course
 
-__version__ = "1.10.0"  # Do not edit before merge to 'main'
+__version__ = "1.10.1"  # Do not edit before merge to 'main'
