baf94cc207511c7f5b3f7e39013648c412b37c4e
Dániel Kántor <git@daniel-kantor.com>
2022-09-25 13:47:13 +0200

    refactor: export course directly using the monorepo

--- scripts/exportYamlCourse.sh
+++ scripts/exportYamlCourse.sh
@@ -3,12 +3,8 @@
 set -euo pipefail
 
 echo -en "⏳ Exporting course $1"
-cd ./apps/librelingo_json_export/ ||
-{
-  echo -en "\r⚠️  Wrong folder structure"
-  exit 1
-}
-if poetry run librelingo-export-cli ../../courses/"$1" ../../apps/web/src/courses/"$1"; then
+
+if poetry run librelingo-export-cli ./courses/"$1" ./apps/web/src/courses/"$1"; then
 		echo -en "\r✅ Exported course $1"
 else
 		echo -en "\r⚠️  Couldn't export course $1"
