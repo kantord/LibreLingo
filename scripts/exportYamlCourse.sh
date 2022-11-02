3bb2ef54db94b9b4d988659e9723af851af401a3
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2022-11-02 09:04:52 +0100

    fix(json-export): remove duplicated character in progress output (#2534)
    * style: add missing space
    
    * style: use \033[K

--- scripts/exportYamlCourse.sh
+++ scripts/exportYamlCourse.sh
@@ -9,7 +9,7 @@ cd ./apps/librelingo_json_export/ ||
   exit 1
 }
 if poetry run librelingo-export-cli ../../courses/"$1" ../../apps/web/src/courses/"$1"; then
-		echo -en "\r✅ Exported course $1"
+		echo -en "\r\033[K✅ Exported course $1"
 else
 		echo -en "\r⚠️  Couldn't export course $1"
 		exit 1
