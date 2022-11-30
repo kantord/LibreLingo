0248f24fe87fe0b4682d0ce95a046a04014e6745
Daniel Kantor <git@daniel-kantor.com>
2022-11-30 19:10:21 +0100

    feat(web): implement new design for landing page

---
+++ apps/lluis/Hero/Backdrop.svelte
@@ -0,0 +1,48 @@
+<div class="backdrop">
+  <div>
+    <img
+      src="/images/home-background-shape-back.svg"
+      alt=""
+      aria-hidden="true"
+    />
+  </div>
+  <div class="backdrop-front">
+    <img
+      src="/images/home-background-shape-front.svg"
+      alt=""
+      aria-hidden="true"
+    />
+  </div>
+</div>
+
+<style>
+  .backdrop {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 500px;
+  }
+
+  .backdrop > * {
+    width: 100%;
+    position: absolute;
+  }
+
+  .backdrop > * > img {
+    width: 98%;
+  }
+
+  .backdrop-front {
+    top: 10%;
+    left: 2%;
+  }
+
+  .backdrop {
+    width: 100%;
+    pointer-events: none;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+</style>
