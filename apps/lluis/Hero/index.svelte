991b9080f244c893b3134dcb6337f05339923a97
Daniel Kantor <git@daniel-kantor.com>
2022-11-30 19:10:21 +0100

    feat(web): implement new design for landing page

---
+++ apps/lluis/Hero/index.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Backdrop from "./Backdrop.svelte"
+</script>
+
+<div class="hero">
+  <Backdrop />
+  <div class="content">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .hero {
+    position: relative;
+    max-width: var(--hero-max-width);
+    margin: auto;
+    aspect-ratio: 17/10;
+    padding-left: 10%;
+    padding-right: 10%;
+    overflow: hidden;
+  }
+
+  @media screen and (max-width: 992px) {
+    .hero {
+      aspect-ratio: unset;
+    }
+  }
+
+  .content {
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+  }
+</style>
