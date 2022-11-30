abc4411e145e07f60fdb6076ed64bdd419d0a299
Daniel Kantor <git@daniel-kantor.com>
2022-11-30 19:10:21 +0100

    feat(web): implement new design for landing page

---
+++ apps/lluis/NewNavBar/NavBar.svelte
@@ -0,0 +1,20 @@
+<nav class="navbar" data-test-id="site-navbar">
+  <div class="navbar__content container-lg" class:has-middle={$$slots.middle}>
+    <slot name="left" />
+    {#if $$slots.middle}
+      <slot name="middle" />
+    {/if}
+    <slot name="right" />
+  </div>
+</nav>
+
+<style>
+  .navbar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 10;
+    background-color: var(--navbar-background);
+  }
+</style>
---
+++ apps/web/src/components/NewMascot.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let shadow = true
+  export let glow = false
+
+  let imageURL =
+    shadow === true
+      ? "images/mascot-jetpack.svg"
+      : "images/mascot-jetpack-noshadow.svg"
+</script>
+
+<img
+  data-test="mascot-jetpack"
+  src={imageURL}
+  alt="LibreLingo Mascot"
+  class="mascot"
+  class:mascot--glow={glow}
+/>
+
+<style>
+  img {
+    min-width: 200px;
+    object-fit: contain;
+  }
+</style>
