6530ce079ff2ef6f3724755ee9cf873867f0de1d
Daniel Kantor <git@daniel-kantor.com>
2024-05-15 09:40:34 +0200

    remove lluis

--- scripts/exportYamlCourse.sh
+++ scripts/exportYamlCourse.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 echo -en "⏳ Exporting course $1"
-if pdm run export-course ./courses/"$1" ./apps/web/src/courses/"$1"; then
+if pdm run export-course ./courses/"$1" ./apps/web/src/lib/courses/"$1"; then
 		echo -en "\r\033[K✅ Exported course $1"
 else
 		echo -en "\r⚠️  Couldn't export course $1"
