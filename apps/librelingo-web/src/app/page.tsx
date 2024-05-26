7622d6c31e5bbea2fa8e5a9a3f003fb00b97c0f6
Daniel Kantor <git@daniel-kantor.com>
2024-05-26 14:33:04 +0200

    feat: have correct page title on the home page

--- apps/librelingo-web/src/app/page.tsx
+++ apps/librelingo-web/src/app/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'LibreLingo',
+  description: 'LibreLingo is an open source language-learning platform',
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">hello</main>
