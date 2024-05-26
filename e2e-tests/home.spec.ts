08742aeb2b5fc7fb1c40d31e090d0875d34fa6c0
Daniel Kantor <git@daniel-kantor.com>
2024-05-26 15:08:19 +0200

    make tests work

--- e2e-tests/home.spec.ts
+++ e2e-tests/home.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
-  await expect(page).toHaveTitle(/LibreLingo/);
+  await expect(page).toHaveTitle("LibreLingo");
 });
 
