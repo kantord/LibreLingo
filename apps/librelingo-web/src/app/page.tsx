d70d6b9027c39841aade44d8eea857cc8249f24a
Daniel Kantor <git@daniel-kantor.com>
2024-07-27 18:59:22 +0200

    ci: run playwright on github actions
    .
    
    .

--- apps/librelingo-web/src/app/page.tsx
+++ apps/librelingo-web/src/app/page.tsx
@@ -11,7 +11,6 @@ export const metadata: Metadata = {
 
 export default async function Home() {
   const courseData = await listAllCourses()
-  console.log(courseData)
 
   return (
     <>
--- playwright.config.ts
+++ playwright.config.ts
@@ -41,37 +41,11 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+   webServer: {
+     command: 'npm run web-dev',
+     url: 'http://127.0.0.1:3000',
+     reuseExistingServer: !process.env.CI,
+   },
 });
